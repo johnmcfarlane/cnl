2e47d733ec5bca2814d1a1f508ec58b7182e42f1
john <john@john-debian>
2015-09-17 23:59:56 -0700

    fixes classic macro bug in which double-evaluation of parameters occurs

--- src/test_utils.h
+++ src/test_utils.h
@@ -4,16 +4,31 @@
 #include <cassert>
 #include <iostream>
 
-#define ASSERT_EQUAL(A, B) \
-	if ((A) != (B)) { \
-		cout << "Failed: \"" << #A " == " #B << "\", i.e. " << (A) << " != " << (B) << endl; \
-		assert(false); \
+template <typename LHS, typename RHS>
+void assert_equal(const LHS & lhs, const RHS & rhs, char const * lhs_name, char const * rhs_name)
+{
+	if (lhs == rhs)
+	{
+		return;
 	}
 
-#define ASSERT_TRUE(A) \
-	if (!(A)) { \
-		cout << "Failed: (" << #A << ") where (" #A "==" << (A) << ')' << endl; \
-		assert(false); \
+	std::cout << "Failed: \"" << lhs_name << " == " << rhs_name << "\", i.e. " << lhs << " != " << rhs << std::endl;
+	assert(false);
+};
+
+template <typename LHS, typename RHS>
+void assert_true(const LHS & expression, char const * expression_name)
+{
+	if (expression)
+	{
+		return;
 	}
+
+	std::cout << "Failed: (" << expression_name << ") where (" << expression_name << "==" << expression << ')' << std::endl; \
+	assert(false);
+};
+
+#define ASSERT_EQUAL(A, B) assert_equal(A, B, #A, #B)
+#define ASSERT_TRUE(A) assert_true(A, #A)
 
 #endif // _SG14_TEST_UTILS_H
