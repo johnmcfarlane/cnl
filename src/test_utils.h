e03540bb548a104da226311e04d090a59baa70ab
John McFarlane <john@mcfarlane.name>
2015-09-18 07:34:57 -0700

    `ASSERT_` macros now report the correct line in `assert` invocations

--- src/test_utils.h
+++ src/test_utils.h
@@ -5,30 +5,30 @@
 #include <iostream>
 
 template <typename LHS, typename RHS>
-void assert_equal(const LHS & lhs, const RHS & rhs, char const * lhs_name, char const * rhs_name)
+bool assert_equal(const LHS & lhs, const RHS & rhs, char const * lhs_name, char const * rhs_name) noexcept
 {
 	if (lhs == rhs)
 	{
-		return;
+		return true;
 	}
 
 	std::cout << "Failed: \"" << lhs_name << " == " << rhs_name << "\", i.e. " << lhs << " != " << rhs << std::endl;
-	assert(false);
+	return false;
 };
 
 template <typename LHS, typename RHS>
-void assert_true(const LHS & expression, char const * expression_name)
+bool assert_true(const LHS & expression, char const * expression_name) noexcept
 {
 	if (expression)
 	{
-		return;
+		return true;
 	}
 
 	std::cout << "Failed: (" << expression_name << ") where (" << expression_name << "==" << expression << ')' << std::endl; \
-	assert(false);
+	return false;
 };
 
-#define ASSERT_EQUAL(A, B) assert_equal(A, B, #A, #B)
-#define ASSERT_TRUE(A) assert_true(A, #A)
+#define ASSERT_EQUAL(A, B) if (!assert_equal(A, B, #A, #B)) assert(false);
+#define ASSERT_TRUE(A) ig (!assert_true(A, #A)) assert(false);
 
 #endif // _SG14_TEST_UTILS_H
