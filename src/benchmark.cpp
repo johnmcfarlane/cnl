65c8d02afdb683d4fef54c3810a38101df012b34
john <john@john-debian>
2015-09-28 01:23:09 -0700

    benchmarking of more complex functions

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,5 +1,7 @@
 #if defined(RUN_BENCHMARKS)
 
+#include "sample_functions.h"
+
 #include <fixed_point_utils.h>
 
 #include <benchmark/benchmark.h>
@@ -87,50 +89,180 @@ static void div(benchmark::State& state)
 	}
 }
 
-using u4_4 = make_ufixed<4, 4>;
-using u0_8 = make_ufixed<0, 8>;
-using u8_0 = make_ufixed<8, 0>;
-using u8_8 = make_ufixed<8, 8>;
-using u0_16 = make_ufixed<0, 16>;
-using u16_0 = make_ufixed<16, 0>;
-using u16_16 = make_ufixed<16, 16>;
-using u0_32 = make_ufixed<0, 32>;
-using u32_0 = make_ufixed<32, 0>;
-using u32_32 = make_ufixed<32, 32>;
-using u0_64 = make_ufixed<0, 64>;
-using u64_0 = make_ufixed<64, 0>;
+template <typename T>
+static void bm_sqrt(benchmark::State& state)
+{
+	auto input = numeric_limits<T>::max() / 5;
+	while (state.KeepRunning())
+	{
+		escape(&input);
+		auto output = sqrt(input);
+		escape(& output);
+	}
+}
 
-#define FIXED_POINT_BENCHMARK_PARTIAL(fn) \
+template <typename T>
+static void bm_magnitude_squared(benchmark::State& state)
+{
+	auto x = T { 1 };
+	auto y = T { 4 };
+	auto z = T { 9 };
+	while (state.KeepRunning())
+	{
+		escape(&x);
+		escape(&y);
+		escape(&z);
+		auto value = magnitude_squared(x, y, z);
+		escape(& value);
+	}
+}
+
+template <typename T>
+static void bm_magnitude_trunc(benchmark::State& state)
+{
+	auto x = T { 1 };
+	auto y = T { 4 };
+	auto z = T { 9 };
+	while (state.KeepRunning())
+	{
+		escape(&x);
+		escape(&y);
+		escape(&z);
+		auto value = magnitude_trunc(x, y, z);
+		escape(& value);
+	}
+}
+
+template <typename T>
+static void bm_circle_intersect_generic(benchmark::State& state)
+{
+	auto x1 = T { 0 };
+	auto y1 = T { 10 };
+	auto r1 = T { 14 };
+	auto x2 = T { 4 };
+	auto y2 = T { 13 };
+	auto r2 = T { 9 };
+	while (state.KeepRunning())
+	{
+		escape(&x1);
+		escape(&y1);
+		escape(&r1);
+		escape(&x2);
+		escape(&y2);
+		escape(&r2);
+		auto value = circle_intersect_generic(x1, y1, r1, x2, y2, r2);
+		escape(& value);
+	}
+}
+
+template <typename T>
+static void circle_intersect_generic(benchmark::State& state)
+{
+	auto x1 = T { 0 };
+	auto y1 = T { 10 };
+	auto r1 = T { 14 };
+	auto x2 = T { 4 };
+	auto y2 = T { 13 };
+	auto r2 = T { 9 };
+	while (state.KeepRunning())
+	{
+		escape(&x1);
+		escape(&y1);
+		escape(&r1);
+		escape(&x2);
+		escape(&y2);
+		escape(&r2);
+		auto value = circle_intersect_generic(x1, y1, r1, x2, y2, r2);
+		escape(& value);
+	}
+}
+
+template <typename T>
+static void bm_circle_intersect_trunc(benchmark::State& state)
+{
+	auto x1 = T { 0 };
+	auto y1 = T { 10 };
+	auto r1 = T { 14 };
+	auto x2 = T { 4 };
+	auto y2 = T { 13 };
+	auto r2 = T { 9 };
+	while (state.KeepRunning())
+	{
+		escape(&x1);
+		escape(&y1);
+		escape(&r1);
+		escape(&x2);
+		escape(&y2);
+		escape(&r2);
+		auto value = circle_intersect_trunc(x1, y1, r1, x2, y2, r2);
+		escape(& value);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// fixed-point types
+
+using u4_4 = make_ufixed<4, 4>;
+using s3_4 = make_fixed<3, 4>;
+using u8_8 = make_ufixed<8, 8>;
+using s7_8 = make_fixed<7, 8>;
+using u16_16 = make_ufixed<16, 16>;
+using s15_16 = make_fixed<15, 16>;
+using u32_32 = make_ufixed<32, 32>;
+using s31_32 = make_fixed<31, 32>;
+
+////////////////////////////////////////////////////////////////////////////////
+// multi-type benchmark macros
+
+#define FIXED_POINT_BENCHMARK_FLOAT(fn) \
 	BENCHMARK_TEMPLATE1(fn, float); \
 	BENCHMARK_TEMPLATE1(fn, double); \
-	BENCHMARK_TEMPLATE1(fn, long double); \
+	BENCHMARK_TEMPLATE1(fn, long double);
+
+#define FIXED_POINT_BENCHMARK_INT(fn) \
 	BENCHMARK_TEMPLATE1(fn, int8_t); \
 	BENCHMARK_TEMPLATE1(fn, uint8_t); \
-	BENCHMARK_TEMPLATE1(fn, u8_0); \
-	BENCHMARK_TEMPLATE1(fn, u4_4); \
 	BENCHMARK_TEMPLATE1(fn, int16_t); \
 	BENCHMARK_TEMPLATE1(fn, uint16_t); \
-	BENCHMARK_TEMPLATE1(fn, u16_0); \
-	BENCHMARK_TEMPLATE1(fn, u8_8); \
 	BENCHMARK_TEMPLATE1(fn, int32_t); \
 	BENCHMARK_TEMPLATE1(fn, uint32_t); \
-	BENCHMARK_TEMPLATE1(fn, u32_0); \
-	BENCHMARK_TEMPLATE1(fn, u16_16); \
 	BENCHMARK_TEMPLATE1(fn, int64_t); \
-	BENCHMARK_TEMPLATE1(fn, uint64_t); \
+	BENCHMARK_TEMPLATE1(fn, uint64_t);
+
+// types that can store values >= 1
+#define FIXED_POINT_BENCHMARK_FIXED(fn) \
+	BENCHMARK_TEMPLATE1(fn, u4_4); \
+	BENCHMARK_TEMPLATE1(fn, s3_4); \
+	BENCHMARK_TEMPLATE1(fn, u8_8); \
+	BENCHMARK_TEMPLATE1(fn, s7_8); \
+	BENCHMARK_TEMPLATE1(fn, u16_16); \
+	BENCHMARK_TEMPLATE1(fn, s15_16); \
 	BENCHMARK_TEMPLATE1(fn, u32_32); \
-	BENCHMARK_TEMPLATE1(fn, u64_0);
+	BENCHMARK_TEMPLATE1(fn, s31_32);
+
+#define FIXED_POINT_BENCHMARK_REAL(fn) \
+	FIXED_POINT_BENCHMARK_FLOAT(fn); \
+	FIXED_POINT_BENCHMARK_FIXED(fn);
 
 #define FIXED_POINT_BENCHMARK_COMPLETE(fn) \
-	FIXED_POINT_BENCHMARK_PARTIAL(fn); \
-	BENCHMARK_TEMPLATE1(fn, u0_8); \
-	BENCHMARK_TEMPLATE1(fn, u0_16); \
-	BENCHMARK_TEMPLATE1(fn, u0_32); \
-	BENCHMARK_TEMPLATE1(fn, u0_64);
+	FIXED_POINT_BENCHMARK_REAL(fn); \
+	FIXED_POINT_BENCHMARK_INT(fn);
+
+////////////////////////////////////////////////////////////////////////////////
+// benchmark invocations
 
 FIXED_POINT_BENCHMARK_COMPLETE(add);
 FIXED_POINT_BENCHMARK_COMPLETE(sub);
 FIXED_POINT_BENCHMARK_COMPLETE(mul);
-FIXED_POINT_BENCHMARK_PARTIAL(div);
+FIXED_POINT_BENCHMARK_COMPLETE(div);
+
+FIXED_POINT_BENCHMARK_REAL(bm_magnitude_squared);
+
+FIXED_POINT_BENCHMARK_REAL(bm_circle_intersect_generic);
+FIXED_POINT_BENCHMARK_FIXED(bm_circle_intersect_trunc);
+
+// tests involving unoptimized math function, sg14::sqrt
+//FIXED_POINT_BENCHMARK_REAL(bm_sqrt);
+//FIXED_POINT_BENCHMARK_FIXED(bm_magnitude_trunc);
 
 #endif	// RUN_BENCHMARKS
