f2370be05e39b3ff880e684a28d5ac9ed6492f48
John McFarlane <mcfarlane.john+github@github.com>
2015-10-25 21:47:36 -0700

    improvements to test_utils.h
    - ASSERT_THROWS - test throwing behavior
    - program exits in release mode
    - error output to cerr

--- src/test_utils.h
+++ src/test_utils.h
@@ -2,33 +2,70 @@
 #define _SG14_TEST_UTILS_H
 
 #include <cassert>
+#include <iomanip>
 #include <iostream>
 
+#define FAIL_STREAM std::cerr
+
+inline void test_break()
+{
+	exit(1);
+}
+
 template <typename LHS, typename RHS>
-bool assert_equal(const LHS & lhs, const RHS & rhs, char const * lhs_name, char const * rhs_name) noexcept
+void assert_equal(const LHS & lhs, const RHS & rhs, char const * lhs_name, char const * rhs_name, char const * file, int line)
 {
 	if (lhs == rhs)
 	{
-		return true;
+		// test passed
+		return;
 	}
-
-	std::cout << "Failed: \"" << lhs_name << " == " << rhs_name << "\", i.e. " << lhs << " != " << rhs << std::endl;
-	return false;
+	
+	FAIL_STREAM << std::setprecision(100)
+		<< file << ':' << line
+		<< ": Failed: \"" << lhs_name << " == " << rhs_name 
+		<< "\", i.e. " << lhs << " != " << rhs << std::endl;
+	
+	test_break();
 };
 
-template <typename LHS, typename RHS>
-bool assert_true(const LHS & expression, char const * expression_name) noexcept
+template <typename Expression>
+void assert_true(const Expression & expression, char const * expression_name, char const * file, int line)
 {
 	if (expression)
 	{
-		return true;
+		// test passed
+		return;
 	}
+	
+	FAIL_STREAM
+		<< file << ':' << line
+		<< ": Failed: (" << expression_name << ") where (" << expression_name << "==" << expression << ')' << std::endl;
+	
+	test_break();
+}
 
-	std::cout << "Failed: (" << expression_name << ") where (" << expression_name << "==" << expression << ')' << std::endl; \
-	return false;
-};
+template <typename Exception, typename Expression>
+void assert_throws(const Expression & expression, char const * expression_name, char const * file, int line)
+{
+	try {
+		expression();
+		FAIL_STREAM
+			<< file << ':' << line
+			<< ": Failed to throw: \"" << expression_name << '"' << std::endl;
+		
+		test_break();
+	}
+	catch (Exception) {
+		// test passed
+	}
+	catch (...) {
+		FAIL_STREAM << "Threw wrong exception: (" << expression_name << ')' << std::endl;
+	}
+}
 
-#define ASSERT_EQUAL(A, B) if (!assert_equal(A, B, #A, #B)) assert(false);
-#define ASSERT_TRUE(A) ig (!assert_true(A, #A)) assert(false);
+#define ASSERT_EQUAL(A, B) do { assert_equal(A, B, #A, #B, __FILE__, __LINE__); } while (false)
+#define ASSERT_TRUE(EXPR) do { assert_true(EXPR, #EXPR, __FILE__, __LINE__)); } while (false)
+#define ASSERT_THROWS(EXPR, EXCEPTION) do { assert_throws<EXCEPTION>([&](){EXPR;}, #EXPR, __FILE__, __LINE__); } while (false)
 
 #endif // _SG14_TEST_UTILS_H
