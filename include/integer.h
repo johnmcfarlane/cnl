e98bf05bb43d152e107e0296032ecc02ccbdeead
John McFarlane <mcfarlane.john+fp@gmail.com>
2015-12-22 16:24:07 -0700

    fixes obscure problems compiling friend operators with clang 3.6
    - workaround involves providing `sg14::integer<>::data()` accessor

--- include/integer.h
+++ include/integer.h
@@ -34,72 +34,51 @@ namespace sg14 {
   // basic definitions
 #define _SG14_INTEGER_COMPARISON_DEFINE(OP) \
     template <typename LhsRepr, typename LhsOverflowPolicy, typename RhsRepr, typename RhsOverflowPolicy> \
-    constexpr bool operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) { \
-        return lhs.repr OP rhs.repr; } \
+    friend constexpr bool operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) { \
+        return lhs.data() OP rhs.data(); } \
     \
-    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type dummy> \
-    constexpr bool operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) { \
+    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type dummy = 0> \
+    friend constexpr bool operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) { \
         using common_type = typename std::common_type<Lhs, RhsRepr>::type; \
-        return static_cast<common_type>(lhs) OP static_cast<common_type>(rhs.repr); } \
+        return static_cast<common_type>(lhs) OP static_cast<common_type>(rhs.data()); } \
     \
-    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type dummy> \
-    constexpr bool operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs) { \
+    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type dummy = 0> \
+    friend constexpr bool operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs) { \
         using common_type = typename std::common_type<LhsRepr, Rhs>::type; \
-        return static_cast<common_type>(lhs.repr) OP static_cast<common_type>(rhs); }
+        return static_cast<common_type>(lhs.data()) OP static_cast<common_type>(rhs); }
 
 #define _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(OP) \
     template <typename LhsRepr, typename LhsOverflowPolicy, typename RhsRepr, typename RhsOverflowPolicy> \
-    constexpr auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
+    friend constexpr auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
     -> typename sg14::_integer_impl::common_type<integer<LhsRepr, LhsOverflowPolicy>, integer<RhsRepr, RhsOverflowPolicy>>::type { \
-        return lhs.repr OP rhs.repr; } \
+        using Result = typename sg14::_integer_impl::common_type<integer<LhsRepr, LhsOverflowPolicy>, integer<RhsRepr, RhsOverflowPolicy>>::type; \
+        return static_cast<Result>(lhs.data() OP rhs.data()); } \
     \
-    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type dummy> \
-    constexpr auto operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
+    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type dummy = 0> \
+    friend constexpr auto operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
     -> integer<typename sg14::_integer_impl::common_type<Lhs, RhsRepr>::type, RhsOverflowPolicy> { \
-        return lhs OP rhs.repr; } \
+        return lhs OP rhs.data(); } \
     \
-    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type dummy> \
-    constexpr auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs) \
+    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type dummy = 0> \
+    friend constexpr auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs) \
     -> integer<typename sg14::_integer_impl::common_type<LhsRepr, Rhs>::type, LhsOverflowPolicy> { \
-        return lhs OP rhs.repr; }
+        return lhs.data() OP rhs; }
 
-  // basic declarations
-#define _SG14_INTEGER_COMPARISON_DECLARE(PREFIX, OP, DUMMY_DEFAULT) \
+#define _SG14_INTEGER_BIT_SHIFT_DEFINE(OP) \
     template <typename LhsRepr, typename LhsOverflowPolicy, typename RhsRepr, typename RhsOverflowPolicy> \
-    PREFIX bool operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs); \
+    friend constexpr auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
+    -> integer<LhsRepr, LhsOverflowPolicy> { \
+        return lhs.data() OP rhs.data(); } \
     \
-    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type DUMMY_DEFAULT> \
-    PREFIX bool operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs); \
+    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type dummy = 0> \
+    friend constexpr auto operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
+    -> Lhs { \
+        return lhs OP rhs.data(); } \
     \
-    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type DUMMY_DEFAULT> \
-    PREFIX bool operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs);
-
-#define _SG14_INTEGER_BINARY_ARITHMETIC_DECLARE(PREFIX, OP, DUMMY_DEFAULT) \
-    template <typename LhsRepr, typename LhsOverflowPolicy, typename RhsRepr, typename RhsOverflowPolicy> \
-    PREFIX auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
-    -> typename sg14::_integer_impl::common_type<integer<LhsRepr, LhsOverflowPolicy>, integer<RhsRepr, RhsOverflowPolicy>>::type; \
-    \
-    template <typename Lhs, typename RhsRepr, typename RhsOverflowPolicy, typename std::enable_if<std::is_arithmetic<Lhs>::value, int>::type DUMMY_DEFAULT> \
-    PREFIX auto operator OP (const Lhs& lhs, const integer<RhsRepr, RhsOverflowPolicy>& rhs) \
-    -> integer<typename sg14::_integer_impl::common_type<Lhs, RhsRepr>::type, RhsOverflowPolicy>; \
-    \
-    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type DUMMY_DEFAULT> \
-    PREFIX auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs) \
-    -> integer<typename sg14::_integer_impl::common_type<LhsRepr, Rhs>::type, LhsOverflowPolicy>;
-
-  // forward-declarations
-#define _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(OP) \
-    _SG14_INTEGER_COMPARISON_DECLARE(constexpr, OP, dummy = 0)
-
-#define _SG14_INTEGER_BINARY_ARITHMETIC_FORWARD_DECLARE(OP) \
-    _SG14_INTEGER_BINARY_ARITHMETIC_DECLARE(constexpr, OP, dummy = 0)
-
-  // friend declarations
-#define _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(OP) \
-    _SG14_INTEGER_COMPARISON_DECLARE(friend constexpr, OP, dummy)
-
-#define _SG14_INTEGER_BINARY_ARITHMETIC_FRIEND_DECLARE(OP) \
-    _SG14_INTEGER_BINARY_ARITHMETIC_DECLARE(friend constexpr, OP, dummy)
+    template <typename LhsRepr, typename LhsOverflowPolicy, typename Rhs, typename std::enable_if<std::is_arithmetic<Rhs>::value, int>::type dummy = 0> \
+    friend constexpr auto operator OP (const integer<LhsRepr, LhsOverflowPolicy>& lhs, const Rhs& rhs) \
+    -> integer<LhsRepr, LhsOverflowPolicy> { \
+        return lhs.data() OP rhs; }
 
   ////////////////////////////////////////////////////////////////////////////////
   // forward-declarations
@@ -118,18 +97,6 @@ namespace sg14 {
       using type = integer<typename std::common_type<LhsRepr, RhsRepr>::type, OverflowPolicy>;
     };
   };
-
-  _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(==);
-  _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(!=);
-  _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(<);
-  _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(>);
-  _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(<=);
-  _SG14_INTEGER_COMPARISON_FORWARD_DECLARE(>=);
-
-  _SG14_INTEGER_BINARY_ARITHMETIC_FORWARD_DECLARE(+);
-  _SG14_INTEGER_BINARY_ARITHMETIC_FORWARD_DECLARE(-);
-  _SG14_INTEGER_BINARY_ARITHMETIC_FORWARD_DECLARE(*);
-  _SG14_INTEGER_BINARY_ARITHMETIC_FORWARD_DECLARE(/);
 
   ////////////////////////////////////////////////////////////////////////////////
   // overflow detection
@@ -245,23 +212,47 @@ namespace sg14 {
       ////////////////////////////////////////////////////////////////////////////////
       // functions
 
-      template<typename ReprRhs>
-      constexpr integer(const ReprRhs& rhs)
+      template<typename RhsRepr, typename std::enable_if<std::is_integral<RhsRepr>::value, int>::type dummy = 0>
+      constexpr integer(const RhsRepr& rhs)
               :repr(OverflowPolicy{}.template convert<repr_type>(rhs))
       {
       }
 
-      _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(==);
-      _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(!=);
-      _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(<);
-      _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(>);
-      _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(<=);
-      _SG14_INTEGER_COMPARISON_FRIEND_DECLARE(>=);
+      template<typename RhsRepr, typename std::enable_if<! std::is_integral<RhsRepr>::value, int>::type dummy = 0>
+      constexpr explicit integer(const RhsRepr& rhs)
+              :repr(OverflowPolicy{}.template convert<repr_type>(rhs))
+      {
+      }
 
-      _SG14_INTEGER_BINARY_ARITHMETIC_FRIEND_DECLARE(+);
-      _SG14_INTEGER_BINARY_ARITHMETIC_FRIEND_DECLARE(-);
-      _SG14_INTEGER_BINARY_ARITHMETIC_FRIEND_DECLARE(*);
-      _SG14_INTEGER_BINARY_ARITHMETIC_FRIEND_DECLARE(/);
+      template<typename LhsRepr>
+      constexpr explicit operator LhsRepr() const
+      {
+          return repr;
+      }
+
+      constexpr friend integer operator-(const integer& rhs) {
+          return integer(- rhs.repr);
+      }
+
+      _SG14_INTEGER_COMPARISON_DEFINE(==);
+      _SG14_INTEGER_COMPARISON_DEFINE(!=);
+      _SG14_INTEGER_COMPARISON_DEFINE(<);
+      _SG14_INTEGER_COMPARISON_DEFINE(>);
+      _SG14_INTEGER_COMPARISON_DEFINE(<=);
+      _SG14_INTEGER_COMPARISON_DEFINE(>=);
+
+      _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(+);
+      _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(-);
+      _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(*);
+      _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(/);
+
+      _SG14_INTEGER_BIT_SHIFT_DEFINE(>>);
+      _SG14_INTEGER_BIT_SHIFT_DEFINE(<<);
+
+      constexpr repr_type const & data() const
+      {
+          return repr;
+      }
 
   private:
       ////////////////////////////////////////////////////////////////////////////////
@@ -282,20 +273,6 @@ namespace sg14 {
   template<typename Repr>
   using saturated_integer = integer<Repr, saturated_overflow_policy>;
 
-  ////////////////////////////////////////////////////////////////////////////////
-  // sg14::integer friends
-
-  _SG14_INTEGER_COMPARISON_DEFINE(==);
-  _SG14_INTEGER_COMPARISON_DEFINE(!=);
-  _SG14_INTEGER_COMPARISON_DEFINE(<);
-  _SG14_INTEGER_COMPARISON_DEFINE(>);
-  _SG14_INTEGER_COMPARISON_DEFINE(<=);
-  _SG14_INTEGER_COMPARISON_DEFINE(>=);
-
-  _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(+);
-  _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(-);
-  _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(*);
-  _SG14_INTEGER_BINARY_ARITHMETIC_DEFINE(/);
 }
 
 #endif	// defined(_SG14_INTEGER)
