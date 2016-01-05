a77e22451bcd71e05e8bd6f589e6cf1249f33073
John McFarlane <mcfarlane.john+github@github.com>
2016-01-04 23:32:55 -0800

    adds boost license
    - licence file added to root and comment added to all C++ source code
    - grants permission to use fixed_point
    - probably most closely-aligned license to ISO C++
    - work-friendly ([we're hiring!](zoox.recruiterbox.com))

--- src/test/proposal.cpp
+++ src/test/proposal.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright John McFarlane 2015 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file ../../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <fixed_point_utils.h>
 
 #include "sample_functions.h"
