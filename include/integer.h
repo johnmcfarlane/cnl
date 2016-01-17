ebdf40348e14708310162c8b9892166c5d58ea25
John McFarlane <mcfarlane.john+github@github.com>
2016-01-17 07:22:42 -0800

    adds missing copyright notice ti integer.h

--- include/integer.h
+++ include/integer.h
@@ -1,3 +1,9 @@
+
+//          Copyright John McFarlane 2015 - 2016.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #if !defined(_SG14_INTEGER)
 #define _SG14_INTEGER 1
 
