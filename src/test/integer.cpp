3001c9a888511a77666a70cd63cb1f296a467b8a
John McFarlane <fixed-point@john.mcfarlane.name>
2016-02-06 05:07:14 -0800

    moves integer.h from include to src/test
    - file is not part of the proposal
    - it merely exemplifies the ability to use non-built-in types with
      fixed_point

--- src/test/integer.cpp
+++ src/test/integer.cpp
@@ -4,7 +4,7 @@
 //  (See accompanying file ../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <integer.h>
+#include "integer.h"
 
 using namespace sg14;
 using namespace std;
