e8130926570fca33ec7633bdf3847d1349b241e1
John McFarlane <fixed-point@john.mcfarlane.name>
2016-04-04 22:53:14 -0700

    sg14::resize_t<float, N> tests

--- src/test/type_traits.cpp
+++ src/test/type_traits.cpp
@@ -31,3 +31,20 @@ static_assert(is_same<resize_t<std::int32_t, 14>, __int128>::value, "sg14::resiz
 static_assert(is_same<resize_t<std::uint64_t, 15>, unsigned __int128>::value, "sg14::resize_t test failed");
 static_assert(is_same<resize_t<std::int64_t, 16>, __int128>::value, "sg14::resize_t test failed");
 #endif
+
+static_assert(is_same<resize_t<double, 1>, float>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<double, 2>, float>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<long double, 3>, float>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<float, 4>, float>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<double, 5>, double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<long double, 6>, double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<float, 7>, double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<double, 8>, double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<long double, 9>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<float, 10>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<double, 11>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<long double, 12>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<float, 13>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<double, 14>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<long double, 15>, long double>::value, "sg14::resize_t test failed");
+static_assert(is_same<resize_t<float, 16>, long double>::value, "sg14::resize_t test failed");
