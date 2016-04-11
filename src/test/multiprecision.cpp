15625bb0f8b41c35f21ad49ac7dd1f4ef26296ae
John McFarlane <fixed-point@john.mcfarlane.name>
2016-04-10 19:09:28 -0700

    adds basic 64 <-> 128-bit arithmetic tests for elastic_multiprecision
    - tests at multiprecision, fixed_point and elastic levels

--- src/test/multiprecision.cpp
+++ src/test/multiprecision.cpp
@@ -11,8 +11,10 @@
 using sg14::is_integral;
 using sg14::is_signed;
 using sg14::is_unsigned;
+using sg14::fixed_point;
 using sg14::make_signed;
 using sg14::make_unsigned;
+using sg14::multiprecision;
 using sg14::elastic_multiprecision;
 using sg14::elasticate;
 using sg14::resize;
@@ -82,7 +84,165 @@ static_assert(width<unsigned_multiprecision<128>>::value>=width<resize<unsigned_
 ////////////////////////////////////////////////////////////////////////////////
 // sg14::multiprecision arithmetic
 
-TEST(multiprecision, divide) {
-    auto n = elastic_multiprecision<64, 0, false>{123456789012345678};
-    ASSERT_EQ(n / n, 1);
+TEST(multiprecision, add)
+{
+    using int64 = unsigned_multiprecision<64>;
+
+    auto augend = int64{123456789012345678LL};
+    auto addend = int64{876543210987654321LL};
+
+    auto sum = augend+addend;
+    auto expected = int64{999999999999999999LL};
+
+    ASSERT_EQ(sum, expected);
+}
+
+TEST(multiprecision, subtract)
+{
+    using int64 = sg14::unsigned_multiprecision<64>;
+
+    auto minuend = int64{999999999999999999LL};
+    auto subtrahend = int64{876543210987654321LL};
+
+    auto difference = minuend-subtrahend;
+    auto expected = int64{123456789012345678LL};
+
+    ASSERT_EQ(difference, expected);
+}
+
+TEST(multiprecision, multiply)
+{
+    using int64 = unsigned_multiprecision<64>;
+    using int128 = unsigned_multiprecision<128>;
+
+    auto factor = int64{123456789012345678LL};
+
+    auto product = static_cast<int128>(factor)*static_cast<int128>(factor);
+    ASSERT_GT(product, factor);
+
+    auto quotient = product/factor;
+    ASSERT_EQ(factor, quotient);
+}
+
+TEST(multiprecision, divide)
+{
+    using int64 = unsigned_multiprecision<64>;
+
+    auto div = int64{123456789012345678LL};
+
+    auto quotient = div/div;
+    auto expected = 1;
+
+    ASSERT_EQ(quotient, expected);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// sg14::fixed_point<sg14::unsigned_multiprecision<>> arithmetic
+
+TEST(fixed_point_multiprecision, add)
+{
+    using int64 = fixed_point<unsigned_multiprecision<64>>;
+
+    auto augend = int64{123456789012345678LL};
+    auto addend = int64{876543210987654321LL};
+
+    auto sum = augend+addend;
+    auto expected = int64{999999999999999999LL};
+
+    ASSERT_EQ(sum, expected);
+}
+
+TEST(fixed_point_multiprecision, subtract)
+{
+    using int64 = fixed_point<unsigned_multiprecision<64>>;
+
+    auto minuend = int64{999999999999999999LL};
+    auto subtrahend = int64{876543210987654321LL};
+
+    auto difference = minuend-subtrahend;
+    auto expected = int64{123456789012345678LL};
+
+    ASSERT_EQ(difference, expected);
+}
+
+TEST(fixed_point_multiprecision, multiply)
+{
+    using int64 = fixed_point<unsigned_multiprecision<64>>;
+    using int128 = fixed_point<unsigned_multiprecision<128>>;
+
+    auto factor = int64{123456789012345678LL};
+
+    auto product = static_cast<int128>(factor)*static_cast<int128>(factor);
+    ASSERT_GT(product, factor);
+
+    auto quotient = product/factor;
+    ASSERT_EQ(factor, quotient);
+}
+
+TEST(fixed_point_multiprecision, divide)
+{
+    using int64 = fixed_point<unsigned_multiprecision<64>>;
+
+    auto div = int64{123456789012345678LL};
+
+    auto quotient = div/div;
+    auto expected = 1;
+
+    ASSERT_EQ(quotient, expected);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// sg14::elastic_multiprecision arithmetic
+
+TEST(elastic_multiprecision, add)
+{
+    using int64 = elastic_multiprecision<64, 0, false>;
+
+    auto augend = int64{123456789012345678LL};
+    auto addend = int64{876543210987654321LL};
+
+    auto sum = augend+addend;
+    auto expected = int64{999999999999999999LL};
+
+    ASSERT_EQ(sum, expected);
+}
+
+TEST(elastic_multiprecision, subtract)
+{
+    using int64 = elastic_multiprecision<64, 0, false>;
+
+    auto minuend = int64{999999999999999999LL};
+    auto subtrahend = int64{876543210987654321LL};
+
+    auto difference = minuend-subtrahend;
+    auto expected = int64{123456789012345678LL};
+
+    ASSERT_EQ(difference, expected);
+}
+
+TEST(elastic_multiprecision, multiply)
+{
+    using int64 = elastic_multiprecision<64, 0, false>;
+    using int128 = elastic_multiprecision<128, 0, false>;
+
+    auto factor = int64{123456789012345678LL};
+
+    auto product = factor*factor;
+    static_assert(std::is_same<int128, decltype(product)>::value, "");
+    ASSERT_GT(product, factor);
+
+    auto quotient = product/factor;
+    ASSERT_EQ(factor, quotient);
+}
+
+TEST(elastic_multiprecision, divide)
+{
+    using int64 = elastic_multiprecision<64, 0, false>;
+
+    auto div = int64{123456789012345678LL};
+
+    auto quotient = div/div;
+    auto expected = 1;
+
+    ASSERT_EQ(quotient, expected);
 }
