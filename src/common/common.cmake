46b212cd68b0af630ef0bc096b0f4da88a37ab90
John McFarlane <mcfarlane.john+github@github.com>
2016-04-23 12:03:38 -0700

    added a profiling flag to cmake scripts
    - forces inclusion of frame pointer for better symbol information

--- src/common/common.cmake
+++ src/common/common.cmake
@@ -8,11 +8,17 @@ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 
   set(EXCEPTION_ENABLED_FLAGS "/EHsc")
   set(EXCEPTION_DISABLED_FLAGS "")
+  set(PROFILE_ENABLED_FLAGS "/Oy-")
+  set(PROFILE_DISABLED_FLAGS "")
+  # NOTE: to omit frame pointer: "/Oy"
 elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang" OR ${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
   set(MISC_FLAGS "-std=c++11 -pthread -Wall -Wextra -Wfatal-errors -Werror")
 
   set(EXCEPTION_ENABLED_FLAGS "-fexceptions -frtti")
   set(EXCEPTION_DISABLED_FLAGS "-fno-exceptions -fno-rtti")
+  set(PROFILE_ENABLED_FLAGS "-fno-omit-frame-pointer")
+  set(PROFILE_DISABLED_FLAGS "")
+  # NOTE: to omit frame pointer: "-fomit-frame-pointer"
 else ()
   message(FATAL_ERROR "unrecognized compiler: ${CMAKE_CXX_COMPILER_ID}")
 endif ()
@@ -24,7 +30,14 @@ else (EXCEPTIONS)
     set(EXCEPTION_FLAGS "${EXCEPTION_DISABLED_FLAGS}")
 endif (EXCEPTIONS)
 
-set(COMMON_CXX_FLAGS "${MISC_FLAGS} ${EXCEPTION_FLAGS}")
+set(PROFILE OFF CACHE BOOL "compile with frame pointer enabled for better profiling information")
+if (PROFILE)
+    set(PROFILE_FLAGS "${PROFILE_ENABLED_FLAGS}")
+else (PROFILE)
+    set(PROFILE_FLAGS "${PROFILE_DISABLED_FLAGS}")
+endif (PROFILE)
+
+set(COMMON_CXX_FLAGS "${MISC_FLAGS} ${EXCEPTION_FLAGS} ${PROFILE_FLAGS}")
 
 include_directories("${CMAKE_CURRENT_LIST_DIR}/../../include")
 include_directories("${CMAKE_CURRENT_LIST_DIR}/.")
