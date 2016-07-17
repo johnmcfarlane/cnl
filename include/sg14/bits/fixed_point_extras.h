1cb3a40439ff2dae34bf9c669b16ffd88ab323d3
John McFarlane <github@john.mcfarlane.name>
2016-07-17 12:51:38 -0700

    std::numeric_limits<fixed_point>::is_integer = false
    - giving up on that idea completely now
    - fixed-point is its own thing

--- include/sg14/bits/fixed_point_extras.h
+++ include/sg14/bits/fixed_point_extras.h
@@ -254,7 +254,7 @@ namespace std {
         //static constexpr int max_digits10 = ?;
 
         static constexpr bool is_signed = _rep_numeric_limits::is_signed;
-        static constexpr bool is_integer = _rep_numeric_limits::is_integer;
+        static constexpr bool is_integer = false;
 
         // TODO: not entirely certain
         static constexpr bool is_exact = true;
