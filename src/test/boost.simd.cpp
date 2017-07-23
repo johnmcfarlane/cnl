86a3d0fef8e10520dbd6fc4a75f34474d851f131
John McFarlane <fixed-point@john.mcfarlane.name>
2017-07-22 23:56:40 -0700

    rename macro: SG14_BOOST_SIMD_ENABLED -> CNL_BOOST_SIMD_ENABLED

--- src/test/boost.simd.cpp
+++ src/test/boost.simd.cpp
@@ -4,7 +4,7 @@
 //  (See accompanying file ../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#if defined(SG14_BOOST_SIMD_ENABLED)
+#if defined(CNL_BOOST_SIMD_ENABLED)
 
 #include <cnl/auxiliary/boost.simd.h>
 
