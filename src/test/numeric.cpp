cf72a185a5ad9a6bab4e707b7a8a9977b0412a3e
John McFarlane <cnl@john.mcfarlane.name>
2017-09-30 11:22:07 -0700

    includes numeric.h at top of numeric.cpp (doh!)

--- src/test/numeric.cpp
+++ src/test/numeric.cpp
@@ -4,6 +4,7 @@
 //    (See accompanying file ../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <cnl/numeric.h>
 #include <cnl/integral_constant.h>
 
 #include <gtest/gtest.h>
