a51a27b2ffd5f92774c42d405ab5a6417ed25444
John McFarlane <cnl@john.mcfarlane.name>
2017-10-15 13:38:52 -0700

    Optimizes scaling of values in examples in zero_cost_average.cpp

--- src/test/zero_cost_average.cpp
+++ src/test/zero_cost_average.cpp
@@ -27,28 +27,23 @@ CONSTEXPR float average_integer(float input1, float input2) {
 
     // user must remember to widen the result to avoid overflow
     auto sum = int64_t{fixed1} + fixed2;
-    auto avg = sum / 2;
 
-    // user must remember that the scale also was squared
-    return avg / 65536.f;
+    // user must elide the two divisions (averaging and scaling) together
+    return sum / (2 * 65536.f);
 }
 
 // the same function using cnl::elastic_integer -
 // a numeric type which widens to avoid overflow
 CONSTEXPR float average_elastic_integer(float input1, float input2) {
-    // use this user-defined literal - shorthand for cnl::constant
-    using namespace literals;
-
     // elastic_integer behaves a lot like native ints
     auto fixed1 = elastic_integer<31>{input1 * 65536.f};
     auto fixed2 = elastic_integer<31>{input2 * 65536.f};
 
     // sum is one bit wider to reduce risk of overflow
     auto sum = fixed1 + fixed2;
-    auto avg = sum / 2_c;
 
     // but the user must still do all the scaling themselves
-    return static_cast<float>(avg) / 65536.f;
+    return sum / (2 * 65536.f);
 }
 
 // the same function using cnl::fixed_point
@@ -59,9 +54,9 @@ CONSTEXPR float average_fixed_point(float input1, float input2) {
 
     // but it uses int under the hood; user must still widen
     auto sum = fixed_point<int64_t, -16>{fixed1} + fixed2;
-    auto avg = sum / 2_c;
 
-    return static_cast<float>(avg);
+    // divide-by 
+    return static_cast<float>(sum >> 1_c);
 }
 
 // finally, the composition of fixed_point and elastic_integer
@@ -77,9 +72,8 @@ CONSTEXPR float average_elastic(float input1, float input2) {
 
     // concise, overflow-resistant and zero-cost!
     auto sum = fixed1 + fixed2;
-    auto avg = sum / 2_elastic;
 
-    return static_cast<float>(avg);
+    return static_cast<float>(sum >> 1_c);
 }
 
 using namespace literals;
