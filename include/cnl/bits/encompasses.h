8d821e4cd65589c54c4154afa96803c8633dffd3
John McFarlane <cnl@john.mcfarlane.name>
2017-11-08 08:52:33 -0700

    enhances cnl::_impl::encompasses to work with non-int types
    - added cnl::_impl::encompasses_upper
      - compliments cnl::_impl::encompasses_lower
    - both encompasses_upper and encompasses_lower default to falst_type for
      their primary template
      - this means that if either cannot compile due to overflow, encompasses
        pessimistically evaluates to false
        - this works very well for safe_integer

--- include/cnl/bits/encompasses.h
+++ include/cnl/bits/encompasses.h
@@ -34,14 +34,15 @@ namespace cnl {
         using unsigned_or_float_t = typename unsigned_or_float<T>::type;
 
         template<class Encompasser, class Encompassed, class Enable = void>
-        struct encompasses_lower;
+        struct encompasses_lower : std::false_type {
+        };
 
         template<class Encompasser, class Encompassed>
         struct encompasses_lower<Encompasser, Encompassed,
                 enable_if_t<numeric_limits<Encompasser>::is_signed
-                        && numeric_limits<Encompassed>::is_signed>> {
-        static constexpr bool value = numeric_limits<Encompasser>::lowest()
-                <=numeric_limits<Encompassed>::lowest();
+                        && numeric_limits<Encompassed>::is_signed>> :
+            std::integral_constant<bool, (numeric_limits<Encompasser>::lowest()
+                <= numeric_limits<Encompassed>::lowest())> {
         };
 
         template<class Encompasser, class Encompassed>
@@ -55,13 +56,17 @@ namespace cnl {
                         && numeric_limits<Encompassed>::is_signed>> : std::false_type {
         };
 
+        template<class Encompasser, class Encompassed>
+        struct encompasses_upper
+        : std::integral_constant<bool, static_cast<unsigned_or_float_t<Encompasser>>(numeric_limits<Encompasser>::max())
+            >= static_cast<unsigned_or_float_t<Encompassed>>(numeric_limits<Encompassed>::max())> {
+        };
+
         // true if Encompassed can be cast to Encompasser without chance of overflow
         template<class Encompasser, class Encompassed>
         struct encompasses {
             static constexpr bool _lower = encompasses_lower<Encompasser, Encompassed>::value;
-            static constexpr bool _upper =
-                    static_cast<unsigned_or_float_t<Encompasser>>(numeric_limits<Encompasser>::max())
-                            >=static_cast<unsigned_or_float_t<Encompassed>>(numeric_limits<Encompassed>::max());
+            static constexpr bool _upper = encompasses_upper<Encompasser, Encompassed>::value;
 
             static constexpr bool value = _lower && _upper;
         };
