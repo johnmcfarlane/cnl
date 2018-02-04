c68bc0b407c4888f22a3d339f2951c9ede32a9e3
John McFarlane <cnl@john.mcfarlane.name>
2018-02-04 14:25:25 -0800

    renames precise_safe_fixed_point to rounding_safe_fixed_point

--- src/test/fixed_point/elastic/precise/overflow/rounding_safe_fixed_point.cpp
+++ src/test/fixed_point/elastic/precise/overflow/rounding_safe_fixed_point.cpp
@@ -10,14 +10,14 @@
 #include <cnl/overflow_integer.h>
 
 namespace cnl {
-    // precise safe fixed-point
+    // rounding safe fixed-point
     template<
             int IntegerDigits,
             int FractionalDigits = 0,
             class OverflowTag = overflow_integer<>::overflow_tag,
             class RoundingTag = rounding_integer<>::rounding,
             class Narrowest = int>
-    using psfp = fixed_point<
+    using rsfp = fixed_point<
             overflow_integer<
                     elastic_integer<
                             IntegerDigits + FractionalDigits,
@@ -36,11 +36,11 @@ namespace cnl {
             class RoundingTag = rounding_integer<>::rounding,
             class Narrowest = int,
             class Input = int>
-    psfp<
+    rsfp<
             numeric_limits<Input>::digits, 0,
             OverflowTag, RoundingTag,
             Narrowest>
-    constexpr make_psfp(Input const& input)
+    constexpr make_rsfp(Input const& input)
     {
         return input;
     }
@@ -51,39 +51,32 @@ namespace cnl {
             class Narrowest = int,
             class Input = int,
             CNL_IMPL_CONSTANT_VALUE_TYPE Value>
-    psfp<
+    rsfp<
             used_bits(Value), -trailing_bits(Value),
             OverflowTag, RoundingTag,
             Narrowest>
-    constexpr make_psfp(constant<Value> const&)
+    constexpr make_rsfp(constant<Value> const&)
     {
         return Value;
     }
 }
 
 namespace {
-    using cnl::make_psfp;
-    using cnl::psfp;
-    using std::is_same;
     using cnl::_impl::identical;
 
     namespace default_parameters {
-        using cnl::rounding_integer;
-        using cnl::overflow_integer;
-        using cnl::elastic_integer;
-
         static_assert(
-                is_same<psfp<1>::rep::rep::rep::rep, int>::value,
+                std::is_same<cnl::rsfp<1>::rep::rep::rep::rep, int>::value,
                 "cnl::rounding_integer parameter default test failed");
     }
 
-    namespace test_make_psfp {
+    namespace test_make_rsfp {
         using namespace cnl::literals;
-        static_assert(identical(make_psfp(cnl::int16{7}), psfp<15>{7}), "");
-        static_assert(identical(make_psfp(444_c), psfp<9, -2>{444}), "");
+        static_assert(identical(cnl::make_rsfp(cnl::int16{7}), cnl::rsfp<15>{7}), "");
+        static_assert(identical(cnl::make_rsfp(444_c), cnl::rsfp<9, -2>{444}), "");
     }
 
     namespace test_multiply {
-        static_assert(identical(cnl::psfp<6>{7}*cnl::psfp<13>{321}, cnl::psfp<19>{2247}), "");
+        static_assert(identical(cnl::rsfp<6>{7}*cnl::rsfp<13>{321}, cnl::rsfp<19>{2247}), "");
     }
 }
