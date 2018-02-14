86eb68b9f2ff7a6097b1d749d39ea7fbb431168c
John McFarlane <cnl@john.mcfarlane.name>
2018-02-13 21:01:35 -0800

    adds <cnl/cmath.h>

---
+++ include/cnl/cmath.h
@@ -0,0 +1,23 @@
+
+//          Copyright John McFarlane 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file
+/// \brief file containing definitions that might go in \verbatim<cmath>\endverbatim if they were standard
+
+#ifndef CNL_CMATH_H
+#define CNL_CMATH_H
+
+#include <cmath>
+
+/// compositional numeric library
+namespace cnl {
+    template<typename T>
+    constexpr T sqrt(T arg) {
+        return std::sqrt(arg);
+    }
+}
+
+#endif //CNL_CMATH_H
