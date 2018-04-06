1de251a34860d72ed7fd4cf979b0224cefb54e27
John McFarlane <github@john.mcfarlane.name>
2018-04-05 19:01:48 -0700

    tmp

--- src/test/vc.cpp
+++ src/test/vc.cpp
@@ -1,3 +1,4 @@
+#define Vc_EXPERIMENTAL 1
 #include <Vc/simd>
 #include <cnl/fixed_point.h>
 
@@ -72,44 +73,56 @@ namespace {
         EXPECT_TRUE(simd_identical(Vc::simd<std::uint16_t>{123}, a));
     }
 
-    ////////////////////////////////////////////////////////////////////////////////
-    // cnl::fixed_point<Vc::simd> tests
-
-    // fixed-point SIMD type
-    template<typename T = int, int Exponent = 0, class Abi = typename Vc::simd<T>::abi_type>
-    using fp_simd = cnl::fixed_point<Vc::simd<T, Abi>, Exponent>;
-
-    TEST(fp_vc, equal) {
-        auto a = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(123));
-        auto b = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(123));
-        EXPECT_TRUE(simd_identical(b, a));
+    template<typename T> using simd = Vc::simd<T, Vc::simd_abi::fixed_size<2>>;
+    TEST(vc, fixed_size) {
+        using simd_float = simd<float>;
+        using simd_uint32_t = simd<std::uint32_t>;
+        auto f = simd_float{std::array<float, 2>{1.5, 16768.0000152587890625}};
+        auto i = simd_uint32_t(f);
     }
-
-    TEST(fp_vc, not_equal) {
-        auto a = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(123));
-        auto b = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(124));
-        EXPECT_TRUE(Vc::all_of(b != a));
-    }
-
-    TEST(fp_vc, add) {
-        std::int16_t a = 1234, b = 8765;
-        auto expected = fp_simd<std::int16_t>(static_cast<std::int16_t>(a*b));
-        auto actual = fp_simd<std::int16_t>(a)*fp_simd<std::int16_t>(b);
-        EXPECT_TRUE(simd_identical(expected, actual));
-    }
-
-    TEST(fp_vc, multiply) {
-        std::int16_t a = 1234, b = 8765;
-        auto expected = fp_simd<std::int16_t>(static_cast<std::int16_t>(a*b));
-        auto actual = fp_simd<std::int16_t>(a)*fp_simd<std::int16_t>(b);
-        EXPECT_TRUE(simd_identical(expected, actual));
-    }
-
-    TEST(fp_vc, constant_shift) {
-        using namespace cnl::literals;
-        std::int64_t a = 1234;
-        auto expected = fp_simd<std::int64_t, 2>(a<<1);
-        auto actual = fp_simd<std::int64_t, 1>(a) << 1_c;
-        EXPECT_TRUE(simd_identical(expected, actual));
-    }
+//
+//    ////////////////////////////////////////////////////////////////////////////////
+//    // cnl::fixed_point<Vc::simd> tests
+//
+//    // fixed-point SIMD type
+//    template<typename T = int, int Exponent = 0, class Abi = typename Vc::simd<T>::abi_type>
+//    using fp_simd = cnl::fixed_point<Vc::simd<T, Abi>, Exponent>;
+//
+//    TEST(fp_vc, equal) {
+//        auto a = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(123));
+//        auto b = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(123));
+//        EXPECT_TRUE(simd_identical(b, a));
+//    }
+//
+//    TEST(fp_vc, not_equal) {
+//        auto a = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(123));
+//        auto b = fp_simd<std::uint16_t>(static_cast<std::uint16_t>(124));
+//        EXPECT_TRUE(Vc::all_of(b != a));
+//    }
+//
+//    TEST(fp_vc, add) {
+//        std::int16_t a = 1234, b = 8765;
+//        auto expected = fp_simd<std::int16_t>(static_cast<std::int16_t>(a*b));
+//        auto actual = fp_simd<std::int16_t>(a)*fp_simd<std::int16_t>(b);
+//        EXPECT_TRUE(simd_identical(expected, actual));
+//    }
+//
+//    TEST(fp_vc, multiply) {
+//        std::int16_t a = 1234, b = 8765;
+//        auto expected = fp_simd<std::int16_t>(static_cast<std::int16_t>(a*b));
+//        auto actual = fp_simd<std::int16_t>(a)*fp_simd<std::int16_t>(b);
+//        EXPECT_TRUE(simd_identical(expected, actual));
+//    }
+//
+//    TEST(fp_vc, constant_shift) {
+//        using namespace cnl::literals;
+//        std::int64_t a = 1234;
+//        auto expected = fp_simd<std::int64_t, 2>(a<<1);
+//        auto actual = fp_simd<std::int64_t, 1>(a) << 1_c;
+//        EXPECT_TRUE(simd_identical(expected, actual));
+//    }
+//
+//    TEST(fp_vc, fixed_size) {
+//        auto a = fp_simd<std::uint32_t, -16, Vc::simd_abi::fixed_size<2>>{{1.5, 16768.0000152587890625}};
+//    }
 }
