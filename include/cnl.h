6f43afa22c5154e9f5782168a2e7925b349c768e
John McFarlane <github@john.mcfarlane.name>
2018-04-10 17:18:20 +0100

    adds cnl.h
    - includes everything else

---
+++ include/cnl.h
@@ -0,0 +1,28 @@
+
+//          Copyright John McFarlane 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file
+/// \brief include-all header for the CNL numeric library
+
+#ifndef CNL_H
+#define CNL_H
+
+#include "cnl/bit.h"
+#include "cnl/cmath.h"
+#include "cnl/cnlint.h"
+#include "cnl/constant.h"
+#include "cnl/elastic_number.h"
+#include "cnl/elastic_integer.h"
+#include "cnl/fixed_point.h"
+#include "cnl/fractional.h"
+#include "cnl/limits.h"
+#include "cnl/numeric.h"
+#include "cnl/num_traits.h"
+#include "cnl/overflow.h"
+#include "cnl/overflow_integer.h"
+#include "cnl/rounding_integer.h"
+
+#endif //CNL_H
