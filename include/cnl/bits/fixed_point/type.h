e1fb61c578d6adca8f8f157a3214282a96568810
John McFarlane <github@john.mcfarlane.name>
2018-05-06 11:12:52 -0700

    Fix multiple inaccuracies in the fixed_point documentation example
    - wrong bit count
    - char might be unsigned

--- include/cnl/bits/fixed_point/type.h
+++ include/cnl/bits/fixed_point/type.h
@@ -86,7 +86,7 @@ namespace cnl {
     ///
     /// \par Examples
     ///
-    /// To define a fixed-point value 1 byte in size with a sign bit, 3 integer bits and 4 fractional bits:
+    /// To define a fixed-point value 1 byte in size with a sign bit, 4 integer bits and 3 fractional bits:
     /// \snippet snippets.cpp define a fixed_point value
 
     template<typename Rep, int Exponent, int Radix>
