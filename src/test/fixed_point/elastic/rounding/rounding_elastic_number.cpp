148477d940877129a826fb75cd50425228f6772d
John McFarlane <github@john.mcfarlane.name>
2018-05-28 11:41:06 -0700

    Fix tests in rounding_elastic_number
    - overflow caused by initializing 63-bit fixed-point number with int
    - the old cnl::divide is renamed cnl::quotient and not directly related to
      the new cnl::divide

--- src/test/fixed_point/elastic/rounding/rounding_elastic_number.cpp
+++ src/test/fixed_point/elastic/rounding/rounding_elastic_number.cpp
@@ -26,7 +26,7 @@ namespace {
     namespace test_addition_larger {
         static constexpr auto lhs = rounding_elastic_number<32>{1};
         static constexpr auto rhs = rounding_elastic_number<32, -30>{1};
-        static constexpr auto expected = rounding_elastic_number<63, -30>{2};
+        static constexpr auto expected = rounding_elastic_number<63, -30>{2LL};
         static constexpr auto sum = lhs + rhs;
         static_assert(identical(expected, sum), "larger rounding_elastic_number addition");
     }
@@ -35,37 +35,25 @@ namespace {
 
         static_assert(identical(
                 rounding_elastic_number<16, -8>{.5},
-                cnl::divide<cnl::nearest_rounding_tag,
-                        rounding_elastic_number<8, 0>,
-                        rounding_elastic_number<8, 0> >()(rounding_elastic_number<8, 0>{1},
-                                                          rounding_elastic_number<8, 0>{2})),
-                      "cnl::divide(rounding_elastic_number, rounding_elastic_number)");
+                cnl::quotient(rounding_elastic_number<8, 0>{1}, rounding_elastic_number<8, 0>{2})),
+                      "cnl::quotient(rounding_elastic_number, rounding_elastic_number)");
 
         static_assert(identical(
                 rounding_elastic_number<62, -31>{2.},
-                cnl::divide<cnl::nearest_rounding_tag,
-                        rounding_elastic_number<31, 0>,
-                        rounding_elastic_number<31, 0> >()(rounding_elastic_number<31, 0>{2},
-                                                           rounding_elastic_number<31, 0>{1})),
-                      "cnl::divide(rounding_elastic_number, rounding_elastic_number)");
+                cnl::quotient(rounding_elastic_number<31, 0>{2}, rounding_elastic_number<31, 0>{1})),
+                      "cnl::quotient(rounding_elastic_number, rounding_elastic_number)");
 
 #if defined(CNL_INT128_ENABLED)
         static_assert(identical(
                 rounding_elastic_number<96, -48>{2.},
-                cnl::divide<cnl::nearest_rounding_tag,
-                        rounding_elastic_number<48, 0>,
-                        rounding_elastic_number<48, 0> >()(rounding_elastic_number<48, 0>{2},
-                                                           rounding_elastic_number<48, 0>{1})),
-                      "cnl::divide(rounding_elastic_number, rounding_elastic_number)");
+                cnl::quotient(rounding_elastic_number<48, 0>{2}, rounding_elastic_number<48, 0>{1})),
+                          "cnl::quotient(rounding_elastic_number, rounding_elastic_number)");
 #endif
 
         static_assert(identical(
                 rounding_elastic_number<16, -8>{.5},
-                cnl::divide<cnl::nearest_rounding_tag,
-                        rounding_elastic_number<8, -4>,
-                        rounding_elastic_number<8, -4> >()(rounding_elastic_number<8, -4>{1},
-                                                           rounding_elastic_number<8, -4>{2})),
-                      "cnl::divide(rounding_elastic_number, rounding_elastic_number)");
+                cnl::quotient(rounding_elastic_number<8, -4>{1}, rounding_elastic_number<8, -4>{2})),
+                      "cnl::quotient(rounding_elastic_number, rounding_elastic_number)");
 
         static_assert(identical(
                 rounding_elastic_number<16, -8>{.5},
