2a66c1ef494cddf7a0648448b388577399cfb249
John McFarlane <github@john.mcfarlane.name>
2018-05-29 04:35:43 -0700

    Correctly implement cnl::scale<N, 2, rounding_integer<>>

--- include/cnl/rounding_integer.h
+++ include/cnl/rounding_integer.h
@@ -154,6 +154,15 @@ namespace cnl {
         }
     };
 
+    template<int Digits, class Rep, class RoundingTag>
+    struct scale<Digits, 2, rounding_integer<Rep, RoundingTag>, _impl::enable_if_t<0<=Digits>> {
+        constexpr auto operator()(rounding_integer<Rep, RoundingTag> const& s) const
+        -> decltype(from_rep<rounding_integer<Rep, RoundingTag>>{}(scale<Digits, 2, Rep>{}(to_rep(s))))
+        {
+            return from_rep<rounding_integer<Rep, RoundingTag>>{}(scale<Digits, 2, Rep>{}(to_rep(s)));
+        }
+    };
+
     ////////////////////////////////////////////////////////////////////////////////
     // cnl::make_rounding_integer
 
