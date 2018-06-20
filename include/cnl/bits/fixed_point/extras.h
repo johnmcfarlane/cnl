6fa3dc05d083dc1e80f27b472cb724b0830f833b
John McFarlane <github@john.mcfarlane.name>
2018-06-20 07:17:37 +0100

    Fix compiler error in sqrt with boost.multiprecision Rep
    - has no comparison with cnl::constant

--- include/cnl/bits/fixed_point/extras.h
+++ include/cnl/bits/fixed_point/extras.h
@@ -109,7 +109,7 @@ namespace cnl {
     {
         using type = fixed_point<Rep, Exponent, Radix>;
         using widened_rep = set_digits_t<Rep, digits<Rep>::value*2>;
-        return to_rep(x)<constant<0>{}
+        return to_rep(x)<0
                ? _impl::terminate<type>("negative value passed to cnl::sqrt")
                : type{from_rep<type>{}(_impl::for_rep<widened_rep>(
                         _impl::fp::extras::sqrt_solve1(),
