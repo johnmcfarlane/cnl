59266c7893a6ac15d525242bd874b032a01bb524
John McFarlane <github@john.mcfarlane.name>
2018-07-07 12:42:45 +0100

    Remove cnl::multiply
    - was already removed from P0037
    - isn't used in CDSP as far as I can tell

--- src/test/fixed_point/fixed_point_common.h
+++ src/test/fixed_point/fixed_point_common.h
@@ -539,13 +539,10 @@ namespace test_arithmetic {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// cnl::multiply
+// multiplication
 
 static_assert(cnl::numeric_limits<uint8>::max()/5==51, "");
 static_assert(cnl::numeric_limits<uint8>::max()/3==85, "");
-
-static_assert(identical(multiply(fixed_point<uint8, -4>{2}, fixed_point<uint8, -4>{7.5}), fixed_point<uint16, -8>{15}),
-        "cnl::multiply test failed");
 
 namespace test_divide {
     static_assert(
@@ -655,30 +652,20 @@ static_assert(identical(-16776450.564086914f, 765.432f-fixed_point<int64, -32>(1
         "cnl::fixed_point subtraction test failed");
 
 // multiplication
-static_assert(identical(multiply(fixed_point<uint8>{0x55}, fixed_point<uint8>{2}), fixed_point<uint16, 0>{0xaa}),
-        "cnl::fixed_point test failed");
 static_assert(identical(fixed_point<uint8>{0x55} * fixed_point<uint8>{2}, fixed_point<test_int, 0>{0xaa}),
         "cnl::fixed_point test failed");
 
-static_assert(identical(multiply(fixed_point<int32, -16>{123.75}, fixed_point<int32, -16>{44.5}), fixed_point<int64, -32>{5506.875}),
-        "cnl::fixed_point test failed");
 static_assert(identical(fixed_point<int64, -16>{123.75} * fixed_point<int32, -16>(44.5), fixed_point<int64, -32>{5506.875}),
         "cnl::fixed_point test failed");
 
-static_assert(identical(multiply(fixed_point<int8, -5>{2.125}, fixed_point<int8, -5>{-3.25}), fixed_point<int16, -10>{-6.90625}),
-        "cnl::fixed_point multiplication test failed");
 static_assert(
         identical(fixed_point<int8, -5>{2.125} * fixed_point<int8, -5>{-3.25}, fixed_point<test_int, -10>{-6.90625}),
         "cnl::fixed_point multiplication test failed");
 
 #if defined(__clang__)
-static_assert(identical(multiply(fixed_point<uint8, 10>{10240}, 3u), fixed_point<uint64, 10>{30720}),
-        "cnl::fixed_point multiplication test failed");
 static_assert(identical(fixed_point<uint8, 10>{10240} * 3u, fixed_point<test_unsigned, 10>{30720}),
         "cnl::fixed_point multiplication test failed");
 
-static_assert(identical(multiply(3u, fixed_point<uint8, 10>{10240}), fixed_point<uint64, 10>{30720}),
-        "cnl::fixed_point multiplication test failed");
 static_assert(identical(3u * uint8{4}, test_unsigned{12}), "cnl::fixed_point multiplication test failed");
 static_assert(identical(3u * fixed_point<uint8, 10>{10240}, fixed_point<test_unsigned, 10>{30720}),
         "cnl::fixed_point multiplication test failed");
