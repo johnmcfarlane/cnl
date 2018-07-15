1ba2bc0535658fa24a978335dec09b051cba31de
John McFarlane <github@john.mcfarlane.name>
2018-07-15 11:34:50 +0100

    Add missing standard includes to single-header list

--- src/single_header/standard_includes.h
+++ src/single_header/standard_includes.h
@@ -1,10 +1,13 @@
 
-#include <cmath>
-#include <utility>
-#include <istream>
+#include <algorithm>
+#include <array>
 #include <climits>
+#include <cmath>
 #include <cstdint>
+#include <istream>
 #include <limits>
+#include <numeric>
 #include <stdexcept>
 #include <type_traits>
+#include <utility>
 
