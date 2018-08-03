059b7665427b4bdac89361d419dde94d3a407a6d
John McFarlane <mcfarlane.john+fixed-point@gmail.com>
2018-08-03 08:20:47 +0100

    Rearrange contents of cnl/overflow.h
    - arranged code by tag with common code in separate file, all under
      cnl/_impl/overflow/ subdirectory

---
+++ include/cnl/_impl/overflow/throwing.h
@@ -0,0 +1,50 @@
+
+//          Copyright John McFarlane 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(CNL_IMPL_OVERFLOW_THROWING_H)
+#define CNL_IMPL_OVERFLOW_THROWING_H
+
+#include "common.h"
+#include "../throw_exception.h"
+
+#include <stdexcept>
+
+/// compositional numeric library
+namespace cnl {
+    // terminate program with diagnostic when overflow is detected
+    // throw an exception when overflow is don overflowetected
+    static constexpr struct throwing_overflow_tag {
+    } throwing_overflow{};
+
+    namespace _impl {
+        template<typename Result>
+        struct positive_overflow_result<Result, throwing_overflow_tag> {
+            constexpr Result operator()() const
+            {
+                return throw_exception<Result, std::overflow_error>("positive overflow");
+            }
+        };
+
+        template<typename Result>
+        struct negative_overflow_result<Result, throwing_overflow_tag> {
+            constexpr Result operator()() const
+            {
+                return throw_exception<Result, std::overflow_error>("negative overflow");
+            }
+        };
+
+        template<typename Result>
+        struct convert<throwing_overflow_tag, Result> : overflow_convert<throwing_overflow_tag, Result> {
+        };
+
+        template<class Operator>
+        struct tagged_binary_operator<throwing_overflow_tag, Operator>
+                : tagged_binary_overflow_operator<throwing_overflow_tag, Operator> {
+        };
+    }
+}
+
+#endif  // CNL_IMPL_OVERFLOW_THROWING_H
