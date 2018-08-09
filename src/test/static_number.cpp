e65a85796e23ca07fd4bf9e2dd272fe144677ef9
John McFarlane <mcfarlane.john+fixed-point@gmail.com>
2018-08-09 22:59:10 +0100

    Add function, cnl::to_chars(char*, char*, fixed_point)
    - a bit of a work in progress

--- src/test/static_number.cpp
+++ src/test/static_number.cpp
@@ -8,6 +8,9 @@
 
 #include <gtest/gtest.h>
 
+#include <array>
+#include <string>
+
 namespace {
     using cnl::_impl::identical;
     using cnl::_impl::assert_same;
@@ -43,6 +46,42 @@ namespace {
         static_assert(identical(cnl::make_static_number(cnl::int16{7}), cnl::static_number<15>{7}), "");
         static_assert(identical(cnl::make_static_number(444_c), cnl::static_number<7, 2>{444}), "");
     }
+
+    TEST(static_number, to_chars)
+    {
+        auto const num_chars = 12;
+        auto buffer = std::array<char, num_chars+1>{};
+        auto chars_first = buffer.data();
+        auto chars_last = chars_first+num_chars;
+        auto result = cnl::to_chars(
+                chars_first,
+                chars_last,
+                cnl::static_number<24, -8>{-45678.765625});
+        ASSERT_EQ(std::errc{}, result.ec);
+        ASSERT_EQ(chars_last, result.ptr);
+        *chars_last = '\0';
+
+        ASSERT_EQ(std::string{"-45678.76562"}, chars_first);
+    }
+
+#if defined(CNL_INT128_ENABLED)
+    TEST(static_number, to_chars_wider)
+    {
+        auto const num_chars = 12;
+        auto buffer = std::array<char, num_chars+1>{};
+        auto chars_first = buffer.data();
+        auto chars_last = chars_first+num_chars;
+        auto result = cnl::to_chars(
+                chars_first,
+                chars_last,
+                cnl::static_number<28, -8>{937016.765625});
+        ASSERT_EQ(std::errc{}, result.ec);
+        ASSERT_EQ(chars_last, result.ptr);
+        *chars_last = '\0';
+
+        ASSERT_EQ(std::string{"937016.76562"}, chars_first);
+    }
+#endif
 
     namespace test_multiply {
         static_assert(identical(
