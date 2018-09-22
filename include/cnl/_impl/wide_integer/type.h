418dd0ec8fef8f4d0de5a24aa3297200f1384a52
John McFarlane <github@john.mcfarlane.name>
2018-09-22 13:40:09 +0100

    Extract code to determine wide_integer::rep

--- include/cnl/_impl/wide_integer/type.h
+++ include/cnl/_impl/wide_integer/type.h
@@ -17,6 +17,19 @@
 /// compositional numeric library
 namespace cnl {
     namespace _impl {
+        // wide_integer_rep
+        template<int Digits, typename Narrowest, typename Enable = void>
+        struct wide_integer_rep;
+
+        template<int Digits, typename Narrowest>
+        struct wide_integer_rep<Digits, Narrowest, enable_if_t<(max_digits<Narrowest>::value>=Digits)>>
+                : set_digits<Narrowest, max(Digits, digits<Narrowest>::value)> {
+        };
+
+        template<int Digits, typename Narrowest>
+        using wide_integer_rep_t = typename wide_integer_rep<Digits, Narrowest>::type;
+
+        // wide_integer
         template<int Digits = digits<int>::value, typename Narrowest = int, class Enable = void>
         class wide_integer;
 
@@ -24,10 +37,10 @@ namespace cnl {
         class wide_integer<Digits, Narrowest, enable_if_t<max_digits<Narrowest>::value>=Digits>>
                 : public number_base<
                         wide_integer<Digits, Narrowest>,
-                        set_digits_t<Narrowest, max(Digits, digits<Narrowest>::value)>> {
+                        wide_integer_rep_t<Digits, Narrowest>> {
             using _base = number_base<
                     wide_integer<Digits, Narrowest>,
-                    set_digits_t<Narrowest, max(Digits, digits<Narrowest>::value)>>;
+                    wide_integer_rep_t<Digits, Narrowest>>;
         public:
             using rep = typename _base::rep;
 
