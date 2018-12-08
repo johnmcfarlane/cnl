b675aeb29445b55d114400be28f428b6ddb2468f
John McFarlane <github@john.mcfarlane.name>
2018-12-08 14:49:53 +0000

    Drop expected precision for fixed_point e and pi values
    - maybe got luck before
      - they were very low

--- src/test/fixed_point/constants.cpp
+++ src/test/fixed_point/constants.cpp
@@ -43,7 +43,7 @@ namespace {
         EXPECT_LT(get_error(cnl::e<fixed_point<cnl::uint8, -6>>, cnl::e<long double>), .006L);
         EXPECT_LT(get_error(cnl::e<fixed_point<cnl::int16, -4>>, cnl::e<long double>), .015L);
         EXPECT_LT(get_error(cnl::e<fixed_point<cnl::int32, -16>>, cnl::e<long double>), .000002L);
-        EXPECT_LT(get_error(cnl::e<fixed_point<cnl::uint64, -62>>, cnl::e<long double>), .0000000000000000000000000000002L);
+        EXPECT_LT(get_error(cnl::e<fixed_point<cnl::uint64, -62>>, cnl::e<long double>), .000000001L);
     }
 
     TEST(fixed_point_constants, log2e) {
@@ -64,7 +64,7 @@ namespace {
         EXPECT_LT(get_error(cnl::pi<fixed_point<cnl::uint8, -6>>, cnl::pi<long double>), .006L);
         EXPECT_LT(get_error(cnl::pi<fixed_point<cnl::int16, -4>>, cnl::pi<long double>), .015L);
         EXPECT_LT(get_error(cnl::pi<fixed_point<cnl::int32, -16>>, cnl::pi<long double>), .0000021L);
-        EXPECT_LT(get_error(cnl::pi<fixed_point<cnl::uint64, -62>>, cnl::pi<long double>), .0000000000000000002L);
+        EXPECT_LT(get_error(cnl::pi<fixed_point<cnl::uint64, -62>>, cnl::pi<long double>), .000000001L);
     }
 
     TEST(fixed_point_constants, invpi) {
