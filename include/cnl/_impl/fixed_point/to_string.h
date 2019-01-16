d5dd701cfcf0df3dd36cea11aab1e9a70ab1f009
John McFarlane <github@john.mcfarlane.name>
2019-01-16 23:34:40 +0000

    Use new overload of to_chars in to_string

--- include/cnl/_impl/fixed_point/to_string.h
+++ include/cnl/_impl/fixed_point/to_string.h
@@ -7,7 +7,6 @@
 #ifndef CNL_IMPL_FIXED_POINT_TO_STRING_H
 #define CNL_IMPL_FIXED_POINT_TO_STRING_H
 
-#include "../assert.h"
 #include "type.h"
 #include "to_chars.h"
 
@@ -20,16 +19,8 @@ namespace cnl {
     template<typename Rep, int Exponent>
     std::string to_string(cnl::fixed_point<Rep, Exponent> const& value)
     {
-        constexpr auto num_chars = _impl::max_decimal_digits<cnl::fixed_point<Rep, Exponent>>::value;
-        char chars[num_chars+1];
-
-        auto result = cnl::to_chars(chars, chars+num_chars, value);
-        CNL_ASSERT(result.ptr>std::begin(chars));
-        CNL_ASSERT(result.ptr<std::end(chars));
-        CNL_ASSERT(result.ec==std::errc{});
-
-        *result.ptr = '\0';
-        return std::begin(chars);
+        auto chars = to_chars(value);
+        return chars.data();
     }
 }
 
