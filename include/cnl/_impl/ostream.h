807d88953df4f97f5f754a094ea2c18238b7f9d4
John McFarlane <git@john.mcfarlane.name>
2019-03-27 08:04:03 +0000

    Add std::ostream overload for 128-bit

---
+++ include/cnl/_impl/ostream.h
@@ -0,0 +1,35 @@
+
+//          Copyright John McFarlane 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(CNL_IMPL_OSTREAM_H)
+#define CNL_IMPL_OSTREAM_H 1
+
+#include "to_chars.h"
+#include "../cstdint.h"
+
+#include <array>
+#include <ostream>
+
+/// compositional numeric library
+namespace cnl {
+    namespace _impl {
+#if defined(CNL_INT128_ENABLED)
+        std::ostream& operator<<(std::ostream& out, int128 const n)
+        {
+            std::array<char, 41> line;
+            return out << cnl::_impl::to_chars_natural(std::begin(line), std::end(line), n);
+        }
+
+        std::ostream& operator<<(std::ostream& out, uint128 const n)
+        {
+            std::array<char, 40> line;
+            return out << cnl::_impl::to_chars_natural(std::begin(line), std::end(line), n);
+        }
+#endif
+    }
+}
+
+#endif  // CNL_IMPL_OSTREAM_H
