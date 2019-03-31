4bccdc1edcd1ed305cb376e99182fbc91a1fc3f6
John McFarlane <git@john.mcfarlane.name>
2019-03-31 08:16:55 +0100

    CLion file templates

---
+++ .idea/fileTemplates/includes/C File Header.h
@@ -0,0 +1,11 @@
+#if ($HEADER_COMMENTS)
+
+//          Copyright John McFarlane $YEAR.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file
+/// \brief 
+
+#end
---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,9 @@
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+/// compositional numeric library
+namespace cnl {
+}
+
+#[[#endif]]#  // ${INCLUDE_GUARD}
