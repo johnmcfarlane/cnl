adf19146dab317688acc0c7844e4322338306e00
John McFarlane <git@john.mcfarlane.name>
2019-04-24 08:44:43 +0100

    Add tests of issue #426
    - https://github.com/johnmcfarlane/cnl/issues/426
    - affirmation of results in fixed_point/elastic/rounding/overflow/426.cpp
    - suggested alternative in static_number/426.cpp

---
+++ src/test/static_number/426.cpp
@@ -0,0 +1,40 @@
+#include <cnl/static_number.h>
+
+#include <gtest/gtest.h>
+
+template <
+        int IntegerDigits,
+        int FractionalDigits,
+        class Narrowest>
+using saturated_elastic_fixed_point = cnl::static_number<
+        IntegerDigits + FractionalDigits, -FractionalDigits, cnl::native_rounding_tag, cnl::saturated_overflow_tag>;
+
+using temp_wide_t = saturated_elastic_fixed_point<23, 8, int32_t>;
+using temp_t = saturated_elastic_fixed_point<7, 8, int16_t>;
+
+TEST(_426, half)
+{
+    // non-clipping/non-overflowing conversion
+    temp_wide_t tw = 0.5;
+    temp_t t = tw;
+    ASSERT_EQ(temp_t(0.5), t);
+}
+
+TEST(_426, minus_150)
+{
+    // clipping conversion should be constrained to min/max
+    temp_wide_t tw = -150;
+    temp_t t = tw;
+    temp_t lowest = cnl::numeric_limits<temp_t>::lowest();
+    ASSERT_EQ(lowest, t);
+
+    ASSERT_EQ(-32767, cnl::unwrap(t));
+    ASSERT_EQ(-32767, cnl::unwrap(lowest));
+}
+
+TEST(_426, plus_150)
+{
+    temp_wide_t tw = 150;
+    temp_t t = tw;
+    ASSERT_EQ(cnl::numeric_limits<temp_t>::max(), t);
+}
