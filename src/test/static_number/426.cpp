3635680516dec916f781da73fa88049531a84e5b
John McFarlane <git@john.mcfarlane.name>
2019-05-02 00:05:25 +0100

    Add missing copyright notice

--- src/test/static_number/426.cpp
+++ src/test/static_number/426.cpp
@@ -1,3 +1,9 @@
+
+//          Copyright John McFarlane 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #include <cnl/static_number.h>
 
 #include <gtest/gtest.h>
