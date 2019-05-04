6db466edffc5704434a958e914a9ded451316ed6
John McFarlane <git@john.mcfarlane.name>
2019-05-04 13:56:23 +0100

    Add from_value_recursive
    - means that when component is composite, the resultant type is not
      produced from shallow use of input as the Rep type

---
+++ include/cnl/_impl/num_traits/from_value_recursive.h
@@ -0,0 +1,26 @@
+
+//          Copyright John McFarlane 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef CNL_IMPL_NUM_TRAITS_FROM_VALUE_RECURSIVE
+#define CNL_IMPL_NUM_TRAITS_FROM_VALUE_RECURSIVE
+
+#include "from_rep.h"
+#include "from_value.h"
+
+namespace cnl {
+    namespace _impl {
+        template<class Number, typename Value>
+        struct from_value_recursive {
+            constexpr auto operator()(Value const& value) const
+            -> decltype(cnl::_impl::from_rep<Number>(cnl::_impl::from_value<typename Number::rep>(value)))
+            {
+                return value;
+            }
+        };
+    }
+}
+
+#endif  // CNL_IMPL_NUM_TRAITS_FROM_VALUE_RECURSIVE
