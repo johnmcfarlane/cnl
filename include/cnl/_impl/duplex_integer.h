16e39c948fc95b1f72dad3714cae5f93b4755158
John McFarlane <git@john.mcfarlane.name>
2019-06-09 04:18:47 +0100

    Add missing #includes in _impl/duplex_integer.h

--- include/cnl/_impl/duplex_integer.h
+++ include/cnl/_impl/duplex_integer.h
@@ -7,8 +7,27 @@
 #ifndef CNL_IMPL_DUPLEX_INTEGER_H
 #define CNL_IMPL_DUPLEX_INTEGER_H
 
+#include "duplex_integer/digits.h"
+#include "duplex_integer/add_signedness.h"
+#include "duplex_integer/multiply.h"
+#include "duplex_integer/remove_signedness.h"
 #include "duplex_integer/instantiate_duplex_integer.h"
-#include "duplex_integer/numeric_limits.h"
+#include "duplex_integer/to_rep.h"
+#include "duplex_integer/operators.h"
 #include "duplex_integer/rounding.h"
+#include "duplex_integer/type.h"
+#include "duplex_integer/comparison.h"
+#include "duplex_integer/shift.h"
+#include "duplex_integer/wants_generic_ops.h"
+#include "duplex_integer/is_signed.h"
+#include "duplex_integer/is_duplex_integer.h"
+#include "duplex_integer/modulo.h"
+#include "duplex_integer/divide.h"
+#include "duplex_integer/ctors.h"
+#include "duplex_integer/numeric_limits.h"
+#include "duplex_integer/forward_declaration.h"
+#include "duplex_integer/set_digits.h"
+#include "duplex_integer/set_width.h"
+#include "duplex_integer/to_chars.h"
 
 #endif  // CNL_IMPL_DUPLEX_INTEGER_H
