df002778931ed969b20ff6d69c7afdf94c843816
John McFarlane <git@john.mcfarlane.name>
2019-08-31 12:34:54 +0100

    Add new compilers, C++20 to Travis CI
    - reworked matrix
    - perform two separate test passes in test.sh
      - INT128, BUILD_TYPE and EXCEPTIONS now set in script

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 c++ --version
 cmake --version
-cloc include
-cloc src
-
-cmake \
-  -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
-  -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  -DCNL_DEV=ON -DCNL_STD=${STD} \
-  -DCNL_EXCEPTIONS=${EXCEPTIONS} \
-  -DCNL_INT128=${INT128} \
-  -DCNL_SANITIZE=${SANITIZE} \
-  -G "${GENERATOR}"
-
-cmake --build . -- -j 8
+cloc "${PROJECT_SOURCE_DIR}"/include
+cloc "${PROJECT_SOURCE_DIR}"/src
 
 export LSAN_OPTIONS=verbosity=1:log_threads=1
-ctest --output-on-failure -j 8
 
-./src/benchmark/Benchmark --benchmark_format=csv>result.csv
-./src/benchmark/report.py result.csv
+build_and_test () {
+  cmake \
+    -DCMAKE_BUILD_TYPE="$1" \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+    -DCNL_DEV=ON -DCNL_STD=${STD} \
+    -DCNL_EXCEPTIONS=$2 \
+    -DCNL_INT128=$3 \
+    -DCNL_SANITIZE=${SANITIZE} \
+    -G "${GENERATOR}" \
+    "${PROJECT_SOURCE_DIR}"
+
+  cmake --build . -- -j $NUM_CPUS
+  ctest --output-on-failure -j $NUM_CPUS
+}
+
+ccache -s
+build_and_test Debug OFF OFF
+build_and_test Release ON ON
+ccache -s
+
+src/benchmark/Benchmark --benchmark_format=csv>result.csv
+"${PROJECT_SOURCE_DIR}"/src/benchmark/report.py result.csv
 ls -l src/test src/benchmark/Benchmark
