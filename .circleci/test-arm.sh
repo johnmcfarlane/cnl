d5cdcd6b31eee799fb149dd731f0c7f41640332b
John McFarlane <git@john.mcfarlane.name>
2019-09-01 20:39:53 +0100

    Introduce Conan package manager
    - gtest and google_benchmark dependencies
    - upgrade .appveyor.yml format from 0.1 to 1.0
    - cleaner console output for Circle CI
    - verbose command line switches used
    - more diagnostic info printed in Travic CI runs
    - better syntax colouring in README.md

--- .circleci/test-arm.sh
+++ .circleci/test-arm.sh
@@ -1,10 +1,30 @@
 #!/usr/bin/env bash
 
+set -ex
+
+export CXX=arm-linux-gnueabi-g++-8
+
+apt-get update
+apt-get install --quiet --yes $1 ccache cloc cmake libboost-dev g++-8-arm-linux-gnueabi python3 python3-pip
+pip3 install conan
+
+conan remote add johnmcfarlane/cnl https://api.bintray.com/conan/johnmcfarlane/cnl
+conan profile new default
+cat ~/.conan/profiles/default
+conan profile update settings.arch=armv7 default
+conan profile update settings.compiler=gcc default
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan profile update settings.compiler.version=8 default
+conan profile update settings.os=Linux default
+
 mkdir /tmp/cnl
 cd /tmp/cnl
 
-apt-get update
-apt-get install -y $1 cmake libboost-dev g++-8-arm-linux-gnueabi
-
-export CXX=arm-linux-gnueabi-g++-8
-cmake -DCMAKE_BUILD_TYPE=Release -DCNL_DEV=ON -DCNL_INT128=OFF -DCMAKE_CXX_FLAGS="-march=armv7-a" -DCNL_TEST_VC=OFF  /root/project
+conan install --build=outdated /root/project
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="-march=armv7-a" \
+    -DCNL_DEV=ON -DCNL_INT128=OFF \
+    -DCNL_TEST_VC=OFF \
+    /root/project
+cmake --build . --target Tests -- -j 8
--- .travis/test-linux.sh
+++ .travis/test-linux.sh
@@ -2,15 +2,14 @@
 
 set -e
 
+NUM_CPUS=$(nproc)
+PROJECT_SOURCE_DIR=/cnl
+
 # from https://github.com/i3/i3/blob/42f5a6ce479968a8f95dd5a827524865094d6a5c/.travis.yml
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
 docker run \
-  --env GENERATOR="${GENERATOR}" \
-  --env NUM_CPUS=$(nproc) \
-  --env PROJECT_SOURCE_DIR=/cnl \
-  --env SANITIZE="${SANITIZE}" \
-  --env STD="${STD}" \
   -v "${TRAVIS_BUILD_DIR}":/cnl/ \
   -v ~/.ccache/:/root/.ccache/ \
   -w /ws \
-  johnmcfarlane/cnl_ci:${IMAGE} /cnl/.travis/test.sh
+  johnmcfarlane/cnl_ci:${IMAGE} \
+  /cnl/.travis/test.sh ${STD} "${GENERATOR}" ${NUM_CPUS} "${PROJECT_SOURCE_DIR}"
