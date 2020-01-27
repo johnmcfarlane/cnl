e412234bfa01f0f57e44d5b5e8c909740d969384
John McFarlane <github@john.mcfarlane.name>
2020-01-27 23:29:05 +0000

    Try to recreate mystery Boost/CNL collision
    As reported in #548

--- test/unit/all.cpp
+++ test/unit/all.cpp
@@ -4,4 +4,11 @@
 //  (See accompanying file ../../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include "boost/program_options/detail/value_semantic.hpp"
+//#include <boost/program_options/options_description.hpp>
 #include <cnl/all.h>
+
+int main()
+{
+    return 1;
+}
