2a062b3c1a7665ac955745d9f1443ebda714630b
John McFarlane <git@john.mcfarlane.name>
2020-04-24 00:11:28 +0100

    Remove unnecessary docker login command

--- .travis/test-linux.sh
+++ .travis/test-linux.sh
@@ -5,8 +5,6 @@ set -e
 NUM_CPUS=$(nproc)
 PROJECT_SOURCE_DIR=/cnl
 
-# from https://github.com/i3/i3/blob/42f5a6ce479968a8f95dd5a827524865094d6a5c/.travis.yml
-docker login -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
 docker run \
   -v "${TRAVIS_BUILD_DIR}":/cnl/ \
   -v ~/.ccache/:/root/.ccache/ \
