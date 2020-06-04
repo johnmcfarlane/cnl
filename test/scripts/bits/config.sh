5d2fb8292a0e61c9ebea4d38da2cceb346854872
John McFarlane <github@john.mcfarlane.name>
2020-06-04 14:12:49 +0100

    Remove superfluous config flags
    Both Conan and CMake default build type is _Release_ already.
    No need to re-specify the defaults here.

--- test/scripts/bits/config.sh
+++ test/scripts/bits/config.sh
@@ -14,11 +14,9 @@ cloc "${PROJECT_DIR}"/test
 
 conan install \
   --build=missing \
-  --settings build_type=Release \
   "${PROJECT_DIR}"
 
 cmake \
-  -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
   -DCMAKE_PROJECT_INCLUDE:FILEPATH="$(pwd)"/conan_paths.cmake \
   -G Ninja \
