a8904b14ff0fc7cdcba8a75632a610915e64caf2
Heikki Berg <cdsp.heikki.berg@gmail.com>
2020-06-18 18:06:20 +0300

    Adds the missing division to tie_to_pos_inf_rounding

--- include/cnl/_impl/rounding/tie_to_pos_inf_rounding_tag.h
+++ include/cnl/_impl/rounding/tie_to_pos_inf_rounding_tag.h
@@ -7,6 +7,12 @@
 #if !defined(CNL_IMPL_ROUNDING_TIE_TO_POS_INF_ROUNDING_TAG_H)
 #define CNL_IMPL_ROUNDING_TIE_TO_POS_INF_ROUNDING_TAG_H
 
+#include "../operators/generic.h"
+#include "../operators/native_tag.h"
+#include "is_rounding_tag.h"
+
+#include <type_traits>
+
 /// compositional numeric library
 namespace cnl {
     /// \brief tag to specify tie to positive inf rounding behavior in arithmetic operations
@@ -18,7 +24,64 @@ namespace cnl {
     /// \sa cnl::rounding_integer,
     /// cnl::add, cnl::convert, cnl::divide, cnl::left_shift, cnl::multiply, cnl::subtract,
     /// cnl::tie_to_pos_inf_rounding_tag
-    struct tie_to_pos_inf_rounding_tag {
+    struct tie_to_pos_inf_rounding_tag
+            : _impl::homogeneous_deduction_tag_base, _impl::homogeneous_operator_tag_base {
+    };
+
+    namespace _impl {
+        template<>
+        struct is_rounding_tag<tie_to_pos_inf_rounding_tag> : std::true_type {
+        };
+    }
+
+    template<class Operator, typename Operand>
+    struct unary_operator<Operator, tie_to_pos_inf_rounding_tag, Operand>
+            : unary_operator<Operator, _impl::native_tag, Operand> {
+    };
+
+    template<class Operator, typename Lhs, typename Rhs>
+    struct binary_operator<Operator, tie_to_pos_inf_rounding_tag, tie_to_pos_inf_rounding_tag, Lhs, Rhs>
+            : Operator {
+    };
+
+    template<typename Lhs, typename Rhs>
+    struct binary_operator<_impl::divide_op, tie_to_pos_inf_rounding_tag, tie_to_pos_inf_rounding_tag, Lhs, Rhs> {
+    private:
+        CNL_NODISCARD constexpr auto myabs(Lhs const& lhs) const
+        {
+            return lhs < 0 ? -lhs : lhs;
+        }
+        CNL_NODISCARD constexpr auto step2(Lhs const& lhs, Rhs const& rhs) const
+        {
+            return (lhs < 0)
+            ? -((myabs(lhs) + (rhs - (lhs < 0 ? 1 : 0))/2) / rhs)
+            : (myabs(lhs) + (rhs - (lhs < 0 ? 1 : 0))/2) / rhs;
+        }
+        CNL_NODISCARD constexpr auto step1(Lhs const& lhs, Rhs const& rhs) const
+        {
+            return (rhs < 0) ? step2(-lhs, -rhs) : step2(lhs, rhs);
+        }
+    public:
+        CNL_NODISCARD constexpr auto operator()(Lhs const& lhs, Rhs const& rhs) const
+        -> decltype(lhs/rhs)
+        {
+            return step1(lhs, rhs);
+        }
+    };
+
+    template<class Operator, class RhsTag, typename Lhs, typename Rhs>
+    struct shift_operator<Operator, tie_to_pos_inf_rounding_tag, RhsTag, Lhs, Rhs>
+            : Operator {
+    };
+
+    template<class Operator, typename Rhs>
+    struct pre_operator<Operator, tie_to_pos_inf_rounding_tag, Rhs>
+            : Operator {
+    };
+
+    template<class Operator, typename Rhs>
+    struct post_operator<Operator, tie_to_pos_inf_rounding_tag, Rhs>
+            : Operator {
     };
 }
 
