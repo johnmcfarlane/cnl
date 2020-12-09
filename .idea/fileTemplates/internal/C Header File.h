221b9192e3c503136f2182576c9c26a428f6085d
John McFarlane <github@john.mcfarlane.name>
2020-12-09 01:14:26 +0000

    Remove editor-specific config files
    Some are old and unmaintained. None are necessary.

--- .idea/fileTemplates/internal/C Header File.h
+++
@@ -1,9 +0,0 @@
-#parse("C File Header.h")
-#[[#ifndef]]# ${INCLUDE_GUARD}
-#[[#define]]# ${INCLUDE_GUARD}
-
-/// compositional numeric library
-namespace cnl {
-}
-
-#[[#endif]]#  // ${INCLUDE_GUARD}
