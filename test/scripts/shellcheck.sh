63cf27596494dd9527481c4166059711be6822ff
John McFarlane <github@john.mcfarlane.name>
2020-12-27 20:08:44 +0000

    Make shellcheck script more robust
    - uses `git ls-files` -- instead of `find` -- to ensure that files under
      .git subdirectory are ignored
      - (This becomes a problem if any files under /.git/ match criteria for
        being a script.)
    - uses file mode bits and shebang of script file -- instead of file
      extensions -- to test whether each file is a bash script

--- test/scripts/shellcheck.sh
+++ test/scripts/shellcheck.sh
@@ -9,14 +9,21 @@ PROJECT_DIR=$(
   pwd
 )
 
-find "${PROJECT_DIR}" -name "*.sh" | while read -r TEST; do
-  shellcheck \
-    --check-sourced \
-    --color=always \
-    --external-sources \
-    --severity=info \
-    --shell=bash \
-    "$TEST"
+cd "${PROJECT_DIR}"
+
+git ls-files | while read -r file
+do
+  if [[ -f "$file" && -x "$file" && "$(head --lines 1 "$file")" == "#!/usr/bin/env bash" ]]
+  then
+    shellcheck \
+        --check-sourced \
+        --color=always \
+        --external-sources \
+        --severity=info \
+        --shell=bash \
+        "$file"
+    printf "OK: %s\n" "$file"
+  fi
 done
 
 echo shellcheck success
