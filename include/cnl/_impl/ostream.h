9c4b50bf498380043bbc14ea37dd0ca7d81f039d
John McFarlane <github@john.mcfarlane.name>
2021-01-10 06:47:48 +0000

    Move 128-bit integer overloads of operator<< into global namespace

--- include/cnl/_impl/ostream.h
+++ include/cnl/_impl/ostream.h
@@ -17,12 +17,16 @@
 namespace cnl {
     namespace _impl {
 #if defined(CNL_INT128_ENABLED)
+        /// \brief output-streaming operator for native signed 128-bit integer
+        /// \note must be used in same scope following a `using cnl::operator<<;` directive
         inline std::ostream& operator<<(std::ostream& out, int128 const n)
         {
             std::array<char, 41> line{};
             return out << cnl::_impl::to_chars_natural(std::begin(line), std::end(line), n);
         }
 
+        /// \brief output-streaming operator for native unsigned 128-bit integer
+        /// \note must be used in same scope following a `using cnl::operator<<;` directive
         inline std::ostream& operator<<(std::ostream& out, uint128 const n)
         {
             std::array<char, 40> line{};
@@ -30,6 +34,8 @@ namespace cnl {
         }
 #endif
     }
+
+    using _impl::operator<<;
 }
 
 #endif  // CNL_IMPL_OSTREAM_H
