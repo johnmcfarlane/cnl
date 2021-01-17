88c3b01d9b25d128f6aea66fe6585388810a531e
John McFarlane <github@john.mcfarlane.name>
2021-01-17 11:00:48 +0000

    Remove dependency on custom Conan pacakge
    - benchmark/1.5.0@johnmcfarlane/stable, replaced with benchmark/1.5.2
    - usability improvement: no longer need `conan remote` command

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,7 @@ class CnlConan(ConanFile):
     }
     generators = "cmake_find_package"
     no_copy_source = True
-    requires = "gtest/1.10.0","benchmark/1.5.0@johnmcfarlane/stable"
+    requires = "gtest/1.10.0", "benchmark/1.5.2"
 
     scm = {
         "revision": "main",
