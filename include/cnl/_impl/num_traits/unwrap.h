3cd095347aca270fca28fb8bbbc1dd7ee1941405
John McFarlane <github@john.mcfarlane.name>
2021-02-22 17:55:03 +0000

    Remove SFINAE hooks from unwrap

--- include/cnl/_impl/num_traits/unwrap.h
+++ include/cnl/_impl/num_traits/unwrap.h
@@ -7,18 +7,17 @@
 #if !defined(CNL_IMPL_NUM_TRAITS_UNWRAP_H)
 #define CNL_IMPL_NUM_TRAITS_UNWRAP_H
 
-#include "../type_traits/enable_if.h"
 #include "is_composite.h"
 #include "rep_of.h"
 #include "to_rep.h"
 
 namespace cnl {
     namespace _impl {
-        template<typename Number, typename Enable = void>
+        template<typename Number>
         struct unwrap;
 
         template<typename Number>
-        struct unwrap<Number, enable_if_t<!is_composite<Number>::value>> {
+        requires(!is_composite<Number>::value) struct unwrap<Number> {
             [[nodiscard]] constexpr auto operator()(Number const& number) const
             {
                 return number;
@@ -26,7 +25,7 @@ namespace cnl {
         };
 
         template<typename Number>
-        struct unwrap<Number, enable_if_t<is_composite<Number>::value>> {
+        requires is_composite_v<Number> struct unwrap<Number> {
             [[nodiscard]] constexpr auto operator()(Number const& number) const
             {
                 return unwrap<rep_of_t<Number>>{}(to_rep(number));
