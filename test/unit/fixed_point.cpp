239587eeabe0a59bd746289eb52097a45f977676
John McFarlane <github@john.mcfarlane.name>
2021-03-07 22:51:19 +0000

    Add unit tests for fixed_point and integer

---
+++ test/unit/fixed_point.cpp
@@ -0,0 +1,32 @@
+
+//          Copyright John McFarlane 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file
+/// \brief test of `cnl/fixed_point.h`
+
+#include <cnl/fixed_point.h>
+
+#include <cnl/_impl/cstdint/types.h>
+#include <cnl/fraction.h>
+#include <cnl/scaled_integer.h>
+#include <cnl/wide_integer.h>
+
+#include <utility>
+
+static_assert(cnl::fixed_point<int>);
+static_assert(cnl::fixed_point<unsigned char>);
+static_assert(cnl::fixed_point<bool>);
+static_assert(cnl::fixed_point<cnl::scaled_integer<>>);
+static_assert(cnl::fixed_point<cnl::wide_integer<>>);
+static_assert(!cnl::fixed_point<cnl::fraction<>>);
+static_assert(!cnl::fixed_point<float>);
+static_assert(!cnl::fixed_point<void>);
+static_assert(!cnl::fixed_point<std::pair<int, int>>);
+
+#if defined(CNL_INT128_ENABLED)
+static_assert(cnl::fixed_point<cnl::int128>);
+static_assert(cnl::fixed_point<cnl::uint128>);
+#endif
---
+++ test/unit/integer.cpp
@@ -0,0 +1,32 @@
+
+//          Copyright John McFarlane 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file ../LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+/// \file
+/// \brief test of `cnl/integer.h`
+
+#include <cnl/integer.h>
+
+#include <cnl/_impl/cstdint/types.h>
+#include <cnl/fraction.h>
+#include <cnl/scaled_integer.h>
+#include <cnl/wide_integer.h>
+
+#include <utility>
+
+static_assert(cnl::integer<int>);
+static_assert(cnl::integer<unsigned char>);
+static_assert(cnl::integer<bool>);
+static_assert(!cnl::integer<cnl::scaled_integer<>>);
+static_assert(cnl::integer<cnl::wide_integer<>>);
+static_assert(!cnl::integer<cnl::fraction<>>);
+static_assert(!cnl::integer<float>);
+static_assert(!cnl::integer<void>);
+static_assert(!cnl::integer<std::pair<int, int>>);
+
+#if defined(CNL_INT128_ENABLED)
+static_assert(cnl::integer<cnl::int128>);
+static_assert(cnl::integer<cnl::uint128>);
+#endif
