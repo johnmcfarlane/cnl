6c4986fddb4669a0e83825c1fb0f1c06b10fd3aa
John McFarlane <github@john.mcfarlane.name>
2021-03-07 23:04:01 +0000

    Derive is_integer from _impl::is_integral
    - instead of std::is_integral
    - accounts for inconsistencies in specialisation of std::is_integral for
      non-standard fundamental integer types

--- include/cnl/_impl/type_traits/is_integral.h
+++ include/cnl/_impl/type_traits/is_integral.h
@@ -18,16 +18,18 @@ namespace cnl {
         struct is_integral : std::is_integral<T> {
         };
 
-        template<class T>
-        inline constexpr auto is_integral_v = is_integral<T>::value;
-
 #if defined(CNL_INT128_ENABLED)
         template<>
-        inline constexpr auto is_integral_v<int128> = true;
+        struct is_integral<int128> : std::true_type {
+        };
 
         template<>
-        inline constexpr auto is_integral_v<uint128> = true;
+        struct is_integral<uint128> : std::true_type {
+        };
 #endif
+
+        template<class T>
+        inline constexpr auto is_integral_v = is_integral<T>::value;
 
         template<typename T>
         concept integral = is_integral_v<T>;
