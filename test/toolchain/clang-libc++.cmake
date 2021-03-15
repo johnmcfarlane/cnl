f9226d547ed8a05c9ebd0f6f0e15d0715e1d977d
Heikki Berg <cdsp.heikki.berg@gmail.com>
2021-03-15 16:25:41 +0200

    To make brew installed clang-11 happy on osx

--- test/toolchain/clang-libc++.cmake
+++ test/toolchain/clang-libc++.cmake
@@ -13,7 +13,11 @@ set(INT128_ENABLED_FLAGS "-DCNL_USE_INT128=1")
 set(INT128_DISABLED_FLAGS "-DCNL_USE_INT128=0")
 
 set(SANITIZE_ENABLED_CXX_FLAGS "-fsanitize=address,undefined -fsanitize-trap=undefined -g -O0")
-set(SANITIZE_ENABLED_LINKER_FLAGS "-fsanitize=address,undefined -rtlib=compiler-rt -lgcc_s")
+if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    set(SANITIZE_ENABLED_LINKER_FLAGS "-fsanitize=address,undefined -rtlib=compiler-rt")
+else()
+    set(SANITIZE_ENABLED_LINKER_FLAGS "-fsanitize=address,undefined -rtlib=compiler-rt -lgcc_s")
+endif()
 
 set(TEST_CXX_FLAGS "-Wconversion -Wno-sign-conversion -ftemplate-backtrace-limit=0")
 set(SAMPLE_CXX_FLAGS "-fpermissive -Wno-sign-compare -Wno-strict-overflow")
