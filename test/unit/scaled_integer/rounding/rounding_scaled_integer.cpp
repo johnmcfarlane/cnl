90b78a99c29b9e335c84850f3fc733373e9071f7
John McFarlane <github@john.mcfarlane.name>
2021-03-20 09:18:06 +0000

    Clean up unit test include

--- test/unit/scaled_integer/rounding/rounding_scaled_integer.cpp
+++ test/unit/scaled_integer/rounding/rounding_scaled_integer.cpp
@@ -4,9 +4,11 @@
 //    (See accompanying file ../LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <cnl/_impl/type_traits/identical.h>
 #include <cnl/rounding_integer.h>
 #include <cnl/scaled_integer.h>
+
+#include <cnl/_impl/num_traits/to_rep.h>
+#include <cnl/_impl/type_traits/identical.h>
 
 #include <cinttypes>
 
