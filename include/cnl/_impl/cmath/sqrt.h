56d567b8b81cf7e537b1c262cd4411738d04d8a3
John McFarlane <github@john.mcfarlane.name>
2021-06-09 10:20:20 +0100

    Clarify source of square root algorithm
    Comment was confusing, read as if it was related to the precondition check.

--- include/cnl/_impl/cmath/sqrt.h
+++ include/cnl/_impl/cmath/sqrt.h
@@ -17,12 +17,13 @@ namespace cnl {
     /// \headerfile cnl/cmath.h
     /// \return square root of `x`
     /// \note This function has O(n) complexity where n is the number of significant digits.
+    /// \note The algorithm is taken from
+    /// [here](https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Binary_numeral_system_.28base_2.29).
     /// \pre `x` must be non-negative
 
     template<integer Integer>
     [[nodiscard]] constexpr auto sqrt(Integer const& x)
     {
-        // https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Binary_numeral_system_.28base_2.29
         CNL_ASSERT(x >= Integer{0});
 
         auto root = +Integer{0};
