c793ca40b06464ce421c3bac6163433b9d2cd89c
John McFarlane <github@john.mcfarlane.name>
2021-11-04 23:14:52 +0000

    Refactor GCC toolchains
    - gcc-armv7.cmake includes gcc.cmake
    - removed -D_GLIBCXX_DEBUG and -D_GLIBCXX_DEBUG_PEDANTIC
      from gcc.cmake
    - added -D_GLIBCXX_ASSERTIONS

--- test/toolchain/gcc-armv7.cmake
+++ test/toolchain/gcc-armv7.cmake
@@ -1,10 +1,3 @@
-include("${CMAKE_CURRENT_LIST_DIR}/base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/gcc.cmake")
 
-set(
-    MISC_FLAGS
-    "${MISC_FLAGS} -fconcepts -fconstexpr-ops-limit=1000000000 -ftemplate-backtrace-limit=0 -pthread -Wno-psabi -DCNL_IMPL_ONEROUS_EVALUATION -march=armv7-a"
-)
-
-set(INT128_ENABLED_FLAGS "${INT128_ENABLED_FLAGS} -Wno-pedantic")
-
-set(SANITIZE_ENABLED_CXX_FLAGS "-fsanitize=address,undefined -fsanitize-undefined-trap-on-error -g -O0")
+set(MISC_FLAGS "${MISC_FLAGS} -DCNL_IMPL_ONEROUS_EVALUATION -march=armv7-a")
