0bdf449779d65d361d4244c2dd869a035d886bd8
John McFarlane <github@john.mcfarlane.name>
2021-11-04 23:24:27 +0000

    Refactored clang-libc++.cmake
    - based off of clang.cmake

--- test/toolchain/clang-libc++.cmake
+++ test/toolchain/clang-libc++.cmake
@@ -1,10 +1,4 @@
-include("${CMAKE_CURRENT_LIST_DIR}/base.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/clang.cmake")
 
-set(
-    MISC_FLAGS
-    "${MISC_FLAGS} -ftemplate-backtrace-limit=0 -fconstexpr-backtrace-limit=0 -fconstexpr-steps=1000000000 -DCNL_IMPL_ONEROUS_EVALUATION -stdlib=libc++"
-)
-
+set(MISC_FLAGS "${MISC_FLAGS} -stdlib=libc++")
 set(CMAKE_EXE_LINKER_FLAGS "-lc++ -lc++abi")
-
-set(SANITIZE_ENABLED_CXX_FLAGS "-fsanitize=address,undefined -fsanitize-trap=undefined -g -O0")
