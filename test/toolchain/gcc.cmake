2e5e8b180a164d409e744bf1b7d51a4bfeeca123
John McFarlane <github@john.mcfarlane.name>
2021-11-06 00:46:03 +0000

    Remove unnecessary flag

--- test/toolchain/gcc.cmake
+++ test/toolchain/gcc.cmake
@@ -2,7 +2,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/base.cmake")
 
 set(
     MISC_FLAGS
-    "${MISC_FLAGS} -D_GLIBCXX_ASSERTIONS -Wno-psabi -fconcepts -fconstexpr-ops-limit=1000000000 -pthread"
+    "${MISC_FLAGS} -D_GLIBCXX_ASSERTIONS -Wno-psabi -fconcepts -fconstexpr-ops-limit=1000000000"
 )
 
 set(INT128_ENABLED_FLAGS "${INT128_ENABLED_FLAGS} -Wno-pedantic")
