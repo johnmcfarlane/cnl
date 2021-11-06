87a8952cb50de1aa8e1211240ddb6a838578681c
John McFarlane <github@john.mcfarlane.name>
2021-11-06 14:44:08 +0000

    Only enable _GLIBCXX_ASSERTIONS in Debug builds

--- test/toolchain/gcc.cmake
+++ test/toolchain/gcc.cmake
@@ -2,7 +2,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/base.cmake")
 
 set(
     CNL_CXX_FLAGS
-    "${CNL_CXX_FLAGS} -D_GLIBCXX_ASSERTIONS -Wno-psabi -fconcepts -fconstexpr-ops-limit=1000000000"
+    "${CNL_CXX_FLAGS} -Wno-psabi -fconcepts -fconstexpr-ops-limit=1000000000"
 )
+set(CNL_CXX_FLAGS_DEBUG "-D_GLIBCXX_ASSERTIONS")
 
 set(INT128_ENABLED_FLAGS "${INT128_ENABLED_FLAGS} -Wno-pedantic")
