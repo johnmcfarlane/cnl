5610a60544a4f6ae3cb4c2238e7dd6d7450953e5
John McFarlane <github@john.mcfarlane.name>
2021-11-17 22:51:13 +0000

    Only enable pedantic warnings if int128 is disabled

--- test/toolchain/base.cmake
+++ test/toolchain/base.cmake
@@ -3,7 +3,7 @@
 # CMAKE_CXX_FLAGS_INIT is ignored.
 set(
     CNL_CXX_FLAGS
-    "-Werror -Wall -Wextra -Wpedantic -Wshadow -Wundef -fdiagnostics-color=always -ftemplate-backtrace-limit=0"
+    "-Werror -Wall -Wextra -Wshadow -Wundef -fdiagnostics-color=always -ftemplate-backtrace-limit=0"
 )
 
 set(CMAKE_CXX_COMPILER_LAUNCHER "ccache")
@@ -14,7 +14,7 @@ set(EXCEPTION_DISABLED_FLAGS "-DBOOST_NO_EXCEPTIONS -DBOOST_NO_RTTI -fno-excepti
 # Note: enabling CNL_INT128 and CNL_SANITIZE for Clang may result in link-time failure.
 # Adding linker flags `-rtlib=compiler-rt` (plus `-lgcc_s` on Linux) helps but is not portable.
 set(INT128_ENABLED_FLAGS "-DCNL_USE_INT128=1")
-set(INT128_DISABLED_FLAGS "-DCNL_USE_INT128=0")
+set(INT128_DISABLED_FLAGS "-DCNL_USE_INT128=0 -Wpedantic")
 
 set(SANITIZE_ENABLED_CXX_FLAGS "-fsanitize=address,undefined -fno-sanitize-recover=all")
 set(SANITIZE_ENABLED_LINKER_FLAGS "-fsanitize=address,undefined")
