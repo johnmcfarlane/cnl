5fdfd97014fa09f927fb1098c09d6411d43ff86d
John McFarlane <github@john.mcfarlane.name>
2021-11-23 23:33:57 +0000

    Expand posix-conan-test matrix
    - libc++ and libstdc++ variants of Clang
      - except where libc++ lacks C++20 support
    - more sanitizers
      - why not?

--- test/toolchain/clang-libc++.cmake
+++ test/toolchain/clang-libc++.cmake
@@ -2,3 +2,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/clang.cmake")
 
 set(CNL_CXX_FLAGS "${CNL_CXX_FLAGS} -stdlib=libc++")
 set(CNL_EXE_LINKER_FLAGS "-lc++ -lc++abi")
+
+# https://github.com/google/benchmark/issues/351#issuecomment-285772578
+set(RUN_HAVE_GNU_POSIX_REGEX 0)
+set(RUN_HAVE_POSIX_REGEX 0)
+set(RUN_HAVE_STD_REGEX 0)
