1ae71be697c176e91db8f652ec38c59900afb74e
John McFarlane <github@john.mcfarlane.name>
2021-11-30 22:41:36 +0000

    Add comment justifying disabling of warning in GCC

--- test/toolchain/gcc-head.cmake
+++ test/toolchain/gcc-head.cmake
@@ -1,5 +1,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/gcc.cmake")
 
+# -Warray-bounds and -Wstringop-overread produce false positives in GCC-12
+# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=103483
 set(
     CNL_CXX_FLAGS
     "${CNL_CXX_FLAGS} -Wno-array-bounds -Wno-stringop-overread"
