97a04018fadd8c831a35f7eecf5c7058183782a2
John McFarlane <git@john.mcfarlane.name>
2021-12-25 16:38:58 +0000

    Fix Benchmark's regex selection flags

--- test/toolchain/clang-libc++.cmake
+++ test/toolchain/clang-libc++.cmake
@@ -4,6 +4,6 @@ set(CNL_CXX_FLAGS "${CNL_CXX_FLAGS} -stdlib=libc++")
 set(CNL_EXE_LINKER_FLAGS "-lc++ -lc++abi")
 
 # https://github.com/google/benchmark/issues/351#issuecomment-285772578
-set(RUN_HAVE_GNU_POSIX_REGEX 0)
-set(RUN_HAVE_POSIX_REGEX 0)
-set(RUN_HAVE_STD_REGEX 0)
+set(HAVE_GNU_POSIX_REGEX 0)
+set(HAVE_POSIX_REGEX 0)
+set(HAVE_STD_REGEX 0)
