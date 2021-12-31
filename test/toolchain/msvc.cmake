a8737826006ded1cff769633a2d69474c4fcccbb
John McFarlane <git@john.mcfarlane.name>
2021-12-31 20:46:26 +0000

    Enable sanitizer on MSVC jobs

--- test/toolchain/msvc.cmake
+++ test/toolchain/msvc.cmake
@@ -6,4 +6,6 @@ set(EXCEPTION_DISABLED_FLAGS "/GR- -DBOOST_NO_EXCEPTIONS -DBOOST_NO_RTTI")
 set(INT128_ENABLED_FLAGS "")
 set(INT128_DISABLED_FLAGS "")
 
+set(SANITIZE_ENABLED_CXX_FLAGS "/fsanitize=address  /fsanitize-address-use-after-return")
+
 set(SAMPLE_CXX_FLAGS "/wd4018")
