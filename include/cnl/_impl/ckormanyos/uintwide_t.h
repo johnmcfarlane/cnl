4c6b0c287f2138a420d71cace7d17bffb21fb721
John McFarlane <git@john.mcfarlane.name>
2022-01-18 21:18:13 +0000

    Remove NOLINTBEGIN insertion from update script
    - update uintwide_t.h to remove UTF-8 BOM again

--- include/cnl/_impl/ckormanyos/uintwide_t.h
+++ include/cnl/_impl/ckormanyos/uintwide_t.h
@@ -1,4 +1,4 @@
-﻿///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 //  Copyright Christopher Kormanyos 1999 - 2022.                 //
 //  Distributed under the Boost Software License,                //
 //  Version 1.0. (See accompanying file LICENSE_1_0.txt          //
